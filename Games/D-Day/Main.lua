6470425fc6142c3ca62baf3717c693e5a584dfd8
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-03 00:22:59 +0100

    made less ugly

--- Games/D-Day/Main.lua
+++ Games/D-Day/Main.lua
@@ -1,34 +1,42 @@
 -- // Game: https://www.roblox.com/games/901793731
 
--- made a while ago and just bypasses the ac atm
+-- // Valiant ENV
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
 
-local reps = game.GetService(game, "ReplicatedStorage")
-local weapons = reps.Weapons  
-local clientbullet = reps.Modules.Client["Client_Bullet"]
+-- // Vars
+local RenderStepped = RunService.RenderStepped
+local Heartbeat = RunService.Heartbeat
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
+local Humanoid = Character.WaitForChild(Character, "Humanoid")
+local CurrentCamera = Workspace.CurrentCamera
+local Mouse = LocalPlayer.GetMouse(LocalPlayer)
+local Weapons = ReplicatedStorage.Weapons
+local ClientBullet = ReplicatedStorage.Modules.Client["Client_Bullet"]
 
+-- // Base MT Vars + Funs
 local mt = getrawmetatable(game)
 local backupnamecall = mt.__namecall
 local backupnewindex = mt.__newindex
-local backupindex = mt .__index
-local localplayer = game.GetService(game, "Players").LocalPlayer
-local char = localplayer.Character or localplayer.CharacterAdded.wait(localplayer.CharacterAdded)
-local humanoid = char.WaitForChild(char, "Humanoid")
-local hrp = char.WaitForChild(char, "HumanoidRootPart")
+local backupindex = mt.__index 
 setreadonly(mt, false)
-getgenv().humspoof = {
-    ["WalkSpeed"] = {
-		["Spoof"] = 30,
-		["Def"] = 16
-	},
-    ["JumpPower"] = {
-		["Spoof"] = 50,
-		["Def"] = 50
-	},
+
+getgenv().DDayHax = {}
+DDayHax.SpoofedValues = {
+    Walkspeed = {
+        Spoof = 30,
+        Default = 16,
+    },
+    JumpPower = {
+        Spoof = 50,
+        Default = 50,
+    },
 }
 
--- // AC Bypass
-local function bypassAC1()
-    for i,v in pairs(weapons:GetChildren()) do
+-- // Bypassing the Anti Cheat
+
+local function bypassACPart1()
+    for i,v in pairs(Weapons:GetChildren()) do
         for _,connection in pairs(getconnections(v.ChildAdded)) do 
             connection:Disable()
         end
@@ -69,8 +77,8 @@ local function bypassAC1()
         end
     end
 
-    local map = game.GetService(game, "Workspace").Map
-    local veggie = game.GetService(game, "Workspace").Vegetation
+    local map = Workspace.Map
+    local veggie = Workspace.Vegetation
     for _, connection in pairs(getconnections(map.AncestryChanged)) do
         connection:Disable()
     end
@@ -102,12 +110,12 @@ local function bypassAC1()
     print('AC Part 1: Done!')
     return true
 end
-bypassAC1()
-if not getgenv().bypassAC2 then
+bypassACPart1()
 
+if not getgenv().bypassAC2 then
     mt.__newindex = newcclosure(function(t, i, v)
         if humspoof[i] then
-            return backupnewindex(t, i, getgenv().humspoof[tostring(i)]["Spoof"])
+            return backupnewindex(t, i, getgenv().DDayHax.SpoofedValues[tostring(i)]["Spoof"])
         end
         return backupnewindex(t, i, v)
     end)
@@ -118,8 +126,8 @@ if not getgenv().bypassAC2 then
         elseif t == humanoid and tostring(k) == 'PlatformStand' then
             return false
         end
-        if getgenv().humspoof[tostring(k)] then
-            return getgenv().humspoof[k]["Def"]
+        if getgenv().DDayHax.SpoofedValues[tostring(k)] then
+            return getgenv().DDayHax.SpoofedValues[k]["Def"]
         end
 
         return backupindex(t, k)
@@ -137,36 +145,11 @@ if not getgenv().bypassAC2 then
     print('AC Part 2 done!')
     getgenv().bypassAC2 = true
 end
-getgenv().TeleportDDay = function(cfr)
-    local localplayer = game.GetService(game, "Players").LocalPlayer
-    local char = localplayer.Character or localplayer.CharacterAdded.wait(localplayer.CharacterAdded)
-    local hrp = char.WaitForChild(char, "HumanoidRootPart")
+
+function TeleportDDay(cfr)
+    local hrp = Character.WaitForChild(Character, "HumanoidRootPart")
     for i = 0, 1, 0.1 do
         hrp.CFrame = hrp.CFrame:lerp(cfr, i)
         wait()
     end
 end
---[[ //
-
-for i,v in pairs(getgc()) do 
-    if getfenv(v).script == clientbullet then 
-		for a,x in pairs(debug.getconstants(v)) do 
-            print(a, x)
-            
-			if tostring(x) == "Actual_Helmet" then
-				debug.setconstant(v, a, "no")
-			end
-		end
-		for a,x in pairs(weapons:GetDescendants()) do 
-			if x:IsA("ModuleScript") then 
-                local req = require(x)
-                req.ReloadTime = 0
-				IdleAnimationSpeed = 100
-				FireAnimationSpeed = 100
-				BoltAnimationSpeed = 100
-				ReloadAnimationSpeed = 100
-			end
-		end
-	end
-end
-]]
