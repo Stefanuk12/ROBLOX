f384ceda6fa19184a0513e0760cf71a7e0cc041d
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-05 18:20:24 +0100

    fast vroom

--- Games/Kohls Admin House/Modules/Music Commands/Main.lua
+++ Games/Kohls Admin House/Modules/Music Commands/Main.lua
@@ -1,15 +1,16 @@
 
 local musicTable = game:GetService("HttpService"):JSONDecode(game:HttpGetAsync('https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Kohls%20Admin%20House/Modules/Music%20Commands/MusicTable.json'))
 function testAllSounds()
-    warn('--~~-- Commencing Music Checks - Please Wait --~~--')
+    warn('--~~-- Commencing Music Checks - Allow upto 30 seconds! --~~--')
     for i,v in pairs(musicTable) do
-        local url = game:HttpGetAsync('https://www.roblox.com/library/'..v.SoundId)
-        if string.match(url, 'https://t6.rbxcdn.com/70608418c648be7ac4e323e3294bb059') or string.match(url, 'https://t5.rbxcdn.com/d28c1b5eed271a7aa76f16689e74ca04') then
-            table.remove(musicTable, i)
-            print('Removed:', v.Name)
-        end
+        coroutine.wrap(function()
+            local url = game:HttpGetAsync('https://www.roblox.com/library/'..v.SoundId)
+            if string.match(url, 'https://t6.rbxcdn.com/70608418c648be7ac4e323e3294bb059') or string.match(url, 'https://t5.rbxcdn.com/d28c1b5eed271a7aa76f16689e74ca04') or string.match(url, "This audio asset has been blocked due to copyright violations") then
+                table.remove(musicTable, i)
+                print('Removed:', v.Name)
+            end
+        end)()
     end
-    warn('--~~-- Music Checks Done! --~~--')
 end
 testAllSounds()
 
@@ -23,5 +24,8 @@ game:GetService("Players").LocalPlayer.Chatted:Connect(function(message)
     end
     if string.match(message, ":play ") and musicTable[id] then
         game:GetService("Players"):Chat(":music "..musicTable[id].SoundId)
+    end
+    if string.match(message, ":refreshmusic") then
+        testAllSounds()
     end
 end)
