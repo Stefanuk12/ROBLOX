f2f4a69d21bea4b92fc25a09d27f659a8f1bcd5d
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-05 18:35:37 +0100

    check function

--- Games/Kohls Admin House/Modules/Music Commands/Main.lua
+++ Games/Kohls Admin House/Modules/Music Commands/Main.lua
@@ -1,11 +1,30 @@
 
 local musicTable = game:GetService("HttpService"):JSONDecode(game:HttpGetAsync('https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Kohls%20Admin%20House/Modules/Music%20Commands/MusicTable.json'))
+local badstuff = {
+    'https://t6.rbxcdn.com/70608418c648be7ac4e323e3294bb059',
+    'https://t5.rbxcdn.com/d28c1b5eed271a7aa76f16689e74ca04',
+    'This audio asset has been blocked due to copyright violations.',
+}
+function checkBadSound(url, SoundId)
+    if url then
+        for i,v in pairs(badstuff) do
+            if string.match(url, v) then
+                return true
+            end
+        end
+    end
+    if SoundId then
+        return (game:GetService("MarketplaceService"):GetProductInfo(SoundId, Enum.InfoType.Asset).Name == '(Removed for copyright)')
+    end
+    return false
+end
+
 function testAllSounds()
     warn('--~~-- Commencing Music Checks - Allow upto 30 seconds! --~~--')
     for i,v in pairs(musicTable) do
         coroutine.wrap(function()
             local url = game:HttpGetAsync('https://www.roblox.com/library/'..v.SoundId)
-            if string.match(url, 'https://t6.rbxcdn.com/70608418c648be7ac4e323e3294bb059') or string.match(url, 'https://t5.rbxcdn.com/d28c1b5eed271a7aa76f16689e74ca04') or string.match(url, "This audio asset has been blocked due to copyright violations.") then
+            if checkBadSound(url, v.SoundId) then
                 table.remove(musicTable, i)
                 print('Removed:', v.Name)
             end
