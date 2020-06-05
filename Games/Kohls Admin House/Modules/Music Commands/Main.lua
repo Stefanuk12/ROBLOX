0489389a443b43a28b430b822abd1185d0836a38
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-05 17:38:29 +0100

    epic imrpoved music checks

--- Games/Kohls Admin House/Modules/Music Commands/Main.lua
+++ Games/Kohls Admin House/Modules/Music Commands/Main.lua
@@ -1,31 +1,27 @@
 
 local musicTable = game:GetService("HttpService"):JSONDecode(game:HttpGetAsync('https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Kohls%20Admin%20House/Modules/Music%20Commands/MusicTable.json'))
 function testAllSounds()
-    print('--~~--Commencing Audio Testing--~~--')
+    warn('--~~-- Commencing Music Checks - Please Wait --~~--')
     for i,v in pairs(musicTable) do
-        local Sound = Instance.new("Sound", game:GetService("Lighting"))
-        Sound.SoundId = v.SoundId
-        Sound:Play()
-        wait(0.1)
-        if Sound.TimeLength < 1 then
+        local url = game:HttpGetAsync('https://www.roblox.com/library/'..v.SoundId)
+        if string.match(url, 'https://t6.rbxcdn.com/70608418c648be7ac4e323e3294bb059') then
             table.remove(musicTable, i)
+            print('Removed:', v.Name)
         end
-        Sound:Destroy()
     end
-    print('--~~--Audio Testing Finished!--~~--')
+    warn('--~~-- Music Checks Done! --~~--')
 end
+testAllSounds()
 
 game:GetService("Players").LocalPlayer.Chatted:Connect(function(message)
     local id
     if string.split(message, " ")[2] then id = tonumber(string.split(message, " ")[2]) end
     if string.match(message, ":getmusic") then
-        testAllSounds()
         for i,v in pairs(musicTable) do
             print(i, "|", v.Name)
         end
     end
     if string.match(message, ":play ") and musicTable[id] then
-        testAllSounds()
         game:GetService("Players"):Chat(":music "..musicTable[id].SoundId)
     end
 end)
