ee0a6028c0e687d47e5b9e81b615bd3814031de4
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-05 21:14:13 +0100

    Added Color Changer

---
+++ Games/Kohls Admin House/Modules/Color Changer/Main.lua
@@ -0,0 +1,42 @@
+function paintServer(Colour, Section)
+    -- // Give you the Paint Bucket if you don't have it
+    if not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("PaintBucket") then
+        game:GetService("Players"):Chat(":gear me 18474459")
+        wait(0.5)
+        game:GetService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid"):Equip(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("PaintBucket"))
+    end
+
+    -- // Some Vars
+    local paintRemote = game:GetService("Players").LocalPlayer.Character:FindFirstChild("PaintBucket").Remotes.ServerControls
+    local SelectedColour = Colour
+    local Terrain = game:GetService("Workspace").Terrain["_Game"]
+
+    -- // Rainbow Function
+    local rainbowcolour = Color3.fromHSV(1, 1, 1)
+    local rainbowroad = coroutine.wrap(function()
+        while wait() do
+            local hue = tick() % 5 / 5
+            rainbowcolour = Color3.fromHSV(hue, 1, 1)
+        end
+    end)()
+    
+    -- // Painting Sections
+    if Section == "All" then
+        for i,v in pairs(Terrain:GetDescendants()) do
+            if v:IsA("Part") then
+                paintRemote:InvokeServer("PaintPart", {["Part"] = v, ["Color"] = rainbowcolour})
+            end
+        end
+    else
+        for i,v in pairs(Terrain.Workspace:GetChildren()) do
+            if string.match(v.Name, Section) then
+                for a,x in pairs(v:GetDescendants())
+                    if x:IsA("Part") then
+                        paintRemote:InvokeServer("PaintPart", {["Part"] = x, ["Color"] = rainbowcolour})
+                    end
+                end
+            end
+        end
+    end
+end
+
