4e3b0a5a1889cebc0be62624360582344b65c3a2
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-05 18:52:27 +0100

    var fix

--- Games/Kohls Admin House/Modules/Music Commands/Main.lua
+++ Games/Kohls Admin House/Modules/Music Commands/Main.lua
@@ -21,13 +21,13 @@ end
 
 function testAllSounds()
     warn('--~~-- Commencing Music Checks - Allow upto 30 seconds! --~~--')
-    for i,v in pairs(musicTableHolder) do
+    for i,v in pairs(musicTable) do
         coroutine.wrap(function()
             wait(1)
             local url = game:HttpGetAsync('https://www.roblox.com/library/'..v.SoundId)
             wait(1)
             if checkBadSound(url, v.SoundId) then
-                table.remove(musicTableHolder, i)
+                table.remove(musicTable, i)
                 print('Removed:', v.Name)
             end
         end)()
@@ -39,12 +39,12 @@ game:GetService("Players").LocalPlayer.Chatted:Connect(function(message)
     local id
     if string.split(message, " ")[2] then id = tonumber(string.split(message, " ")[2]) end
     if string.match(message, ":getmusic") then
-        for i,v in pairs(musicTableHolder) do
+        for i,v in pairs(musicTable) do
             print(i, "|", v.Name)
         end
     end
-    if string.match(message, ":play ") and musicTableHolder[id] then
-        game:GetService("Players"):Chat(":music "..musicTableHolder[id].SoundId)
+    if string.match(message, ":play ") and musicTable[id] then
+        game:GetService("Players"):Chat(":music "..musicTable[id].SoundId)
     end
     if string.match(message, ":refreshmusic") then
         getgenv().musicTable = game:GetService("HttpService"):JSONDecode(game:HttpGetAsync('https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Kohls%20Admin%20House/Modules/Music%20Commands/MusicTable.json'))
