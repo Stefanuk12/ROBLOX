d8baa7d19ee5c3f7ddbb5daa8612db2928507e59
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-09 22:06:55 +0100

    to make compatible with other exploits ig

--- Games/Field Trip Z/Main.lua
+++ Games/Field Trip Z/Main.lua
@@ -1,40 +1,26 @@
 -- Game: https://roblox.com/games/4954096313
 
--- // Valiant ENV
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
-
 -- // Vars
-local RenderStepped = RunService.RenderStepped
-local Heartbeat = RunService.Heartbeat
-local LocalPlayer = Players.LocalPlayer
-local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-local Humanoid = Character.WaitForChild(Character, "Humanoid")
-local CurrentCamera = Workspace.CurrentCamera
-local Mouse = LocalPlayer.GetMouse(LocalPlayer)
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RE = ReplicatedStorage:WaitForChild("NetworkEvents"):WaitForChild("RemoteEvent")
 local RF = ReplicatedStorage:WaitForChild("NetworkEvents"):WaitForChild("RemoteFunction")
-local Interactions = Workspace.Interactions
+local Interactions = Workspace:WatForChild("Interactions")
 local Weapons = Interactions:WaitForChild("Weapons")
-local BloodSamples = Interactions.BloodSamples
-local KillParts = Interactions.KillParts
-local Items = Interactions.Items
-local Glass = Interactions.Glass
+local BloodSamples = Interactions:WaitForChild("BloodSamples")
+local KillParts = Interactions:WaitForChild("KillParts")
+local Items = Interactions:WaitForChild("Items")
+local Glass = Interactions:WaitForChild("Glass")
 
 -- // Base MT Vars + Funs
 local mt = getrawmetatable(game)
 local backupnamecall = mt.__namecall
-local backupnewindex = mt.__newindex
-local backupindex = mt.__index 
 setreadonly(mt, false)
 
--- // Anti Kick + Inf DMG + Godmode
-hookfunction(LocalPlayer.Kick, warn)
-hookfunction(LocalPlayer.kick, warn)
+-- // Inf DMG + Godmode
 mt.__namecall = newcclosure(function(...)
     local method = getnamecallmethod()
     local args = {...}
-    if string.lower(method) == "kick" then
-        return nil
-    end
     if method == "InvokeServer" then
         if tostring(args[1]) == "RemoteFunction" then
             if tostring(args[2]) == "DO_DAMAGE" then
@@ -50,9 +36,9 @@ mt.__namecall = newcclosure(function(...)
 end)
 
 -- // Godmode
-RF:InvokeServer("HEAL_PLAYER", LocalPlayer, 100)
 for i,v in pairs(Players:GetPlayers()) do
     local Humanoid = v.Character:FindFirstChildWhichIsA("Humanoid")
+    RF:InvokeServer("HEAL_PLAYER", v, 100)
     Humanoid.HealthChanged:Connect(function(health)
         RF:InvokeServer("HEAL_PLAYER", v, 100)
     end)
