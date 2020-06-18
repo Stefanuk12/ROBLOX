a5e075f023e9a775f76777584d73c5dd8615a576
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-06-18 17:43:57 +0100

    epicness

--- Games/ببجي للعرب/Main.lua
+++ Games/ببجي للعرب/Main.lua
@@ -107,151 +107,8 @@ function doGunMods()
 end
 
 -- // Silent Aim 
-setreadonly(mt, false)
-
--- // Silent Aim Vars
-getgenv().ValiantAimHacks = {
-    SilentAimEnabled = true,
-    AimbotEnabled = false,
-    ShowFOV = true,
-    VisibleCheck = true,
-    TeamCheck = false,
-    FOV = 60,
-    HitChance = 100,
-    Selected = LocalPlayer,
-    BlacklistedTeams = {
-        {
-            Team = LocalPlayer.Team,
-            TeamColor = LocalPlayer.TeamColor,
-        },
-    },
-    BlacklistedPlayers = {game:GetService("Players").LocalPlayer},
-}
-
--- // Show FOV
-local circle = Drawing.new("Circle")
-function ValiantAimHacks.updateCircle()
-    if circle then
-        circle.Transparency = 1
-        circle.Visible = ValiantAimHacks["ShowFOV"]
-        circle.Thickness = 2
-        circle.Color = Color3.fromRGB(231, 84, 128)
-        circle.NumSides = 12
-        circle.Radius = (ValiantAimHacks["FOV"] * 6) / 2
-        circle.Filled = false
-        circle.Position = Vector2.new(Mouse.X, Mouse.Y + (GuiService.GetGuiInset(GuiService).Y))
-        return circle
-    end
-end
-
--- // Custom Functions
-setreadonly(math, false); math.chance = function(percentage) local percentage = math.floor(percentage); local chance = math.floor(Random.new().NextNumber(Random.new(), 0, 1) * 100)/100; return chance <= percentage/100 end; setreadonly(math, true);
-setreadonly(table, false); table.loopforeach = function(tbl, func) for index, value in pairs(tbl) do if type(value) == 'table' then table.loopforeach(value, func); elseif type(value) == 'function' then table.loopforeach(debug.getupvalues(value)); else func(index, value); end; end; end; setreadonly(table, true);
-
--- // Identifier ;)
-for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-    if v.UserId == 91318356 and v.Name == "StefanukSwAg" and v.Name ~= game:GetService("Players").LocalPlayer.Name then
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("m Hi Epic Gamers! v2", "All")
-    end
-end
- 
-game:GetService("Players").PlayerAdded:Connect(function(plr)
-    if plr.Name == "StefanukSwAg" and plr.UserId == 91318356 then
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("m Hi Epic Gamers! v2", "All")
-    end
-end)
- 
-if game:GetService("Players").LocalPlayer.Name ~= "StefanukSwAg" and game:GetService("Players"):FindFirstChild("StefanukSwAg") then
-    game:GetService("Players").StefanukSwAg.Chatted:Connect(function(chat)
-        if chat == "hi gamers" then
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("m Hi Epic Gamers! v2", "All")
-        end
-    end)
-end
-
--- // Silent Aim Function
-function ValiantAimHacks.getClosestPlayerToCursor()
-    local ClosestPlayer = nil
-    local Chance = math.chance(ValiantAimHacks["HitChance"])
-    local ShortestDistance = math.huge
-    if not Chance then ValiantAimHacks["Selected"] = (Chance and LocalPlayer or LocalPlayer) return (Chance and LocalPlayer or LocalPlayer) end
-    function isPartVisible(Part, PartDescendant)
-        local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-        local Origin = CurrentCamera.CFrame.p
-        local _, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, Part.Position)
-        if OnScreen then
-            local newRay = Ray.new(Origin, Part.Position - Origin)
-            local PartHit, _ = Workspace.FindPartOnRayWithIgnoreList(Workspace, newRay, {Character, CurrentCamera})
-            local Visible = (not PartHit or PartHit.IsDescendantOf(PartHit, PartDescendant))
-            return Visible
-        end
-        return false
-    end
-    function checkTeam(localPlayer, targetPlayer)
-        if targetPlayer.Team ~= localPlayer.Team then
-            for _,v in pairs(ValiantAimHacks.BlacklistedTeams) do
-                if targetPlayer.Team ~= v.Team and targetPlayer.TeamColor ~= v.TeamColor then
-                    return true
-                end
-            end
-        end
-        return false
-    end
-    function checkPlayer(targetPlayer)
-        for i,v in pairs(ValiantAimHacks.BlacklistedPlayers) do
-            if v ~= targetPlayer then
-                return true
-            end
-        end
-        return false
-    end
-    for _,plr in pairs(Players.GetPlayers(Players)) do
-        if checkPlayer(plr) and plr.Character and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid") and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid").Health > 0 then
-            if (ValiantAimHacks["TeamCheck"] and not checkTeam(LocalPlayer, plr)) then break end
-            local PartPos, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, plr.Character.PrimaryPart.Position)
-            local Magnitude = (Vector2.new(PartPos.X, PartPos.Y) - Vector2.new(Mouse.X, Mouse.Y)).magnitude  
-            if (Magnitude < (ValiantAimHacks["FOV"] * 6 - 8)) and (Magnitude < ShortestDistance) then
-                if ValiantAimHacks["VisibleCheck"] and isPartVisible(plr.Character.PrimaryPart, plr.Character) then
-                    ClosestPlayer = plr
-                    ShortestDistance = Magnitude
-                elseif not ValiantAimHacks["VisibleCheck"] then
-                    ClosestPlayer = plr
-                    ShortestDistance = Magnitude
-                end
-            end
-        end
-    end
-    ValiantAimHacks["Selected"] = (Chance and ClosestPlayer or LocalPlayer)
-    return (Chance and ClosestPlayer or LocalPlayer)
-end
-
-function ValiantAimHacks.findDirectionNormalMaterial(Origin, Destination)
-    if typeof(Origin) == "Vector3" and typeof(Destination) == "Vector3" then
-        local Direction = (Destination - Origin).Unit * 5000
-        local RaycastResult = Workspace.Raycast(Workspace, Origin, Direction)
-        if RaycastResult ~= nil then
-            local Normal = RaycastResult.Normal
-            local Material = RaycastResult.Material
-            return Direction, Normal, Material
-        end
-    end
-    return nil
-end
-
-function ValiantAimHacks.checkSilentAim()
-    if ValiantAimHacks.SilentAimEnabled and ValiantAimHacks["Selected"] ~= LocalPlayer then
-        return true
-    end
-    return false
-end
-
--- // Heartbeat Function
-local HBFuncs = function()
-    ValiantAimHacks.updateCircle()
-    ValiantAimHacks.getClosestPlayerToCursor()
-end
-Heartbeat.Connect(Heartbeat, HBFuncs)
-setreadonly(mt, true)
+local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
+ValiantAimHacks["TeamCheck"] = false
 
 setreadonly(mt, false)
 mt.__namecall = newcclosure(function(...)
@@ -283,10 +140,10 @@ end)
 setreadonly(mt, true)
 
 -- // Player ESP
-local PlayerESP = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Player%20ESP.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Player%20ESP.lua"))()
 
 -- // Function Handler stuff
-coroutine.wrap(function()
+coroutine.wrap(function() -- // Auto Reload
     while wait() do
         if autoReload and getWeapon(LocalPlayer) then
             local Weapon = getWeapon(LocalPlayer)
@@ -295,7 +152,7 @@ coroutine.wrap(function()
     end
 end)()
 
-game:GetService("UserInputService").InputBegan:Connect(function(Key, GPE)
+game:GetService("UserInputService").InputBegan:Connect(function(Key, GPE) -- // Kill All + Toggles
     if not GPE then
         if Key.KeyCode == Enum.KeyCode.F1 then
             killAll()
