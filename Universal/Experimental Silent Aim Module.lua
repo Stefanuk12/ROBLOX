08860b0714b782fcb63e1851f59c8c556c1f7dac
Stefanuk12 <stefanukpadd@gmail.com>
2020-06-30 00:05:07 +0100

    removed unnessary vars

--- Universal/Experimental Silent Aim Module.lua
+++ Universal/Experimental Silent Aim Module.lua
@@ -3,20 +3,11 @@ if getgenv().ValiantAimHacks then return getgenv().ValiantAimHacks end
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
 
 -- // Vars
-local RenderStepped = RunService.RenderStepped
 local Heartbeat = RunService.Heartbeat
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-local Humanoid = Character.WaitForChild(Character, "Humanoid")
 local CurrentCamera = Workspace.CurrentCamera
 local Mouse = LocalPlayer.GetMouse(LocalPlayer)
-
--- // Base MT Vars + Funs
-local mt = getrawmetatable(game)
-local backupnamecall = mt.__namecall
-local backupnewindex = mt.__newindex
-local backupindex = mt.__index 
---setreadonly(mt, false)
 
 -- // Silent Aim Vars
 getgenv().ValiantAimHacks = {
