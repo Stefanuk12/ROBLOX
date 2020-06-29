8203a02aa86cc085885502333f752ac33088dfa3
Stefanuk12 <stefanukpadd@gmail.com>
2020-06-29 03:19:23 +0100

    update

--- Games/Anomic/Main.lua
+++ Games/Anomic/Main.lua
@@ -1,4 +1,28 @@
 -- Game: https://www.roblox.com/games/4581966615
+--[[
+    Bugs: 
+        1. Silent Aim doesn't work
+    
+    Caution:
+        1. You will get kicked if you rapid fire for long enough. If you want to remove rapid fire then make:
+            local changeVals = {
+                Firemode = "Auto",
+                MaxAmmo = math.huge,
+                Firerate = 2000,
+                Range = 2000000,
+                ReloadTime = 0.1,
+                Accuracy = 0.1,
+                Speed = 250,
+            }
+        into this:
+            local changeVals = {
+                MaxAmmo = math.huge,
+                Range = 2000000,
+                ReloadTime = 0.1,
+                Accuracy = 0.1,
+                Speed = 250,
+            }
+]]
 
 -- // Valiant ENV
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
@@ -23,7 +47,6 @@ local changeVals = {
     ReloadTime = 0.1,
     Accuracy = 0.1,
     Speed = 250,
-    Price = 0,
 }
 local Remotes = game:GetService("ReplicatedStorage")["_CS.Events"]
 
@@ -36,13 +59,12 @@ setreadonly(mt, false)
 
 -- // Get Gun
 function returnGun()
-    for i,v in pairs(Character.GetChildren(Character)) do
-        if v.IsA(v, "Tool") and v.FindFirstChild(v, "MainGunScript") then
-            return v
+    for i,v in pairs(Character:GetChildren()) do
+        if v.IsA(v, "Tool") and v.FindFirstChild(v, "MainGunScript") and v.MainGunScript.FindFirstChild(v.MainGunScript, "FireEvent") then
+            return v.Name
         end
     end
 end
-
 -- // Silent Aim + Anti Kick
 hookfunction(LocalPlayer.Kick, warn)
 hookfunction(LocalPlayer.kick, warn)
@@ -50,13 +72,6 @@ hookfunction(LocalPlayer.kick, warn)
 local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
 ValiantAimHacks["TeamCheck"] = false
 
-mt.__index = newcclosure(function(t, k)
-    if t == Mouse and (k == "Target" or k == "Hit") and ValiantAimHacks.checkSilentAim() then
-        local CPlayer = ValiantAimHacks["Selected"].Character.Head
-        return (k == "Target" and CPlayer or CPlayer.CFrame)
-    end
-    return backupindex(t, k)
-end)
 
 mt.__namecall = newcclosure(function(...)
     local args = {...}
@@ -65,17 +80,21 @@ mt.__namecall = newcclosure(function(...)
         if args[1] == Remotes.RayDrawer and ValiantAimHacks.checkSilentAim() then
             local CPlayer = ValiantAimHacks["Selected"].Character
             args[2] = CPlayer.Head.Position
-            backupnamecall(Remotes.WeaponServer, "Player", CPlayer.Humanoid, returnGun().Name, CPlayer.Head)
+            backupnamecall(Remotes.WeaponServer, "Player", CPlayer.Humanoid, returnGun(), CPlayer.Head)
             return backupnamecall(unpack(args))
         end
-    elseif method == "UserOwnsGamePassAync" and game:GetService("MarketplaceService"):GetProductInfo(tostring(args[2])) then
-        return true
+    elseif method == "Fire" then
+        if tostring(args[1]) == "GunDetailClient" and ValiantAimHacks.checkSilentAim() then
+            args[2] = ValiantAimHacks["Selected"].Character.Head.Position
+            return backupnamecall(unpack(args))
+        end
     elseif string.lower(method) == "kick" then
         return nil
     end
     return backupnamecall(...)
 end)
 print('Done Silent Aim!')
+
 --[[ // Trash AC Bypass
 local connections = {
     Humanoid.GetPropertyChangedSignal(Humanoid, "WalkSpeed"),
@@ -99,60 +118,68 @@ LocalPlayer.CharacterAdded:Connect(removeConnections)]]
 
 -- // Unlimited Stamina
 UIS.InputBegan:Connect(function(Key, GPE)
-    if not GPE and Key.KeyCode == Enum.KeyCode.LeftShift then Sprinting = true end
+    if not GPE then
+        if Key.KeyCode == Enum.KeyCode.LeftShift then Sprinting = true end
+        if Key.KeyCode == Enum.KeyCode.F4 then 
+            ValiantAimHacks["SilentAimEnabled"] = not ValiantAimHacks["SilentAimEnabled"] 
+            ValiantAimHacks["ShowFOV"] = not ValiantAimHacks["ShowFOV"]
+        end       
+    end
 end)
 UIS.InputEnded:Connect(function(Key, GPE)
     if not GPE and Key.KeyCode == Enum.KeyCode.LeftShift then Sprinting = false end
 end)
 
+-- // Gun Mods
+function gunMods()
+    for i,v in pairs(changeVals) do
+        for _,x in pairs(require(ItemList)) do
+            if rawget(x, i) then
+                rawset(x, i, v)
+            end
+        end
+    end
+    print('Done Gun Mods!')
+end
+gunMods()
+
+
+-- // Unlimited Ammo
+function unlimtedAmmo()
+    LocalPlayer.Character.ChildAdded:Connect(function(v)
+        if v.IsA(v, "Tool") and v.FindFirstChild(v, "MainGunScript") then
+            v.Handle.Mag.Value = math.huge
+            print('Done Unlimited Ammo!')
+        end
+    end)
+end
+unlimtedAmmo()
+
+-- // Backpack
+LocalPlayer.PlayerScripts.OwnsBackpackPass.Value = true
+LocalPlayer.CharacterAdded:Connect(function() 
+    LocalPlayer.PlayerScripts:WaitForChild("OwnsBackpackPass").Value = true 
+    print('Got Backpack Gamepass!')
+    unlimtedAmmo()
+end)
+
+-- // Unlimited Stamina
 function unlimitedStamina()
-    for _,v in pairs(getgc()) do
-        if getfenv(v).script and getfenv(v).script.Name == "SprintHandler" then
-            for a,x in pairs(debug.getupvalues(v)) do
-                if type(x) == 'number' then
-                    coroutine.wrap(function()
+    coroutine.wrap(function()
+        for _,v in pairs(getgc()) do
+            if getfenv(v).script and getfenv(v).script.Name == "SprintHandler" then
+                for a,x in pairs(debug.getupvalues(v)) do
+                    if type(x) == 'number' then
                         while wait(0.05) do
                             if Sprinting then
                                 debug.setupvalue(v, a, 100)
                             end
                         end
-                    end)()
+                    end
                 end
             end
         end
-    end
+    end)()
     print('Done Unlimited Stamina!')
 end
 unlimitedStamina()
-
--- // Unlimited Ammo
-coroutine.wrap(function()
-    while wait() do
-        if returnGun() then
-            local Gun = returnGun()
-            if Gun then
-                Gun:WaitForChild("Handle"):WaitForChild("Mag").Value = math.huge
-            end
-        end
-    end
-end)()
-
--- // Gun Mods
-for i,v in pairs(changeVals) do
-    for a,x in pairs(require(ItemList)) do
-        if rawget(x, a) then
-            rawset(x, a, v)
-            print(a,"=",v)
-        end
-    end
-end
-print('Done Gun Mods!')
-
--- // Backpack
-LocalPlayer.PlayerScripts.OwnsBackpackPass.Value = true
-LocalPlayer.CharacterAdded:Connect(function() 
-    wait(1) 
-    unlimitedStamina() 
-    LocalPlayer.PlayerScripts:WaitForChild("OwnsBackpackPass").Value = true 
-    print('Got Backpack Gamepass!')
-end)
