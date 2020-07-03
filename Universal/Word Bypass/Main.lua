b6886c88d8c4f7bd18399da2ca5b713436ca4f10
Stefanuk12 <stefanukpadd@gmail.com>
2020-07-04 00:23:00 +0100

    epic

---
+++ Universal/Word Bypass/Main.lua
@@ -0,0 +1,24 @@
+-- // The Bypass Character
+local SpecialChar1 = "ꜞ"
+local Enabled = true
+
+-- // Base MT Vars
+local mt = getrawmetatable(game)
+local backupnamecall = mt.__namecall
+
+-- // Function that makes text bypassed
+function bypassText(text)
+    return text = string.gsub(text, "(....)", "%1"..SpecialChar1)
+end
+
+-- // MT Itself
+setreadonly(mt, false)
+mt.__namecall = newcclosure(function(...)
+    local args = {...}
+    if Enabled and tostring(args[1]) == "SayMessageRequest" then
+        args[2] = bypassText(args[2])
+        return backupnamecall(unpack(args))
+    end
+    return backupnamecall(unpack(args))
+end)
+setreadonly(mt, true)
