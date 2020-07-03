5ef7a2c91010250d8233bf58d191f8681cfde307
Stefanuk12 <stefanukpadd@gmail.com>
2020-07-04 00:30:45 +0100

    made it remove punctuation

--- Universal/Word Bypass/Main.lua
+++ Universal/Word Bypass/Main.lua
@@ -9,8 +9,11 @@ local mt = getrawmetatable(game)
 local backupnamecall = mt.__namecall
 
 -- // Function that makes text bypassed
-function bypassText(text)
-    return string.gsub(text, "(....)", "%1"..SpecialChar1)
+function bypassText(text, removepunctuation)
+    if removepunctuation then
+        text = string.gsub(text, "[%p]+", "")
+    end
+    return string.gsub(text, "(...)", "%1"..SpecialChar1)
 end
 
 -- // MT Itself
@@ -18,9 +21,11 @@ setreadonly(mt, false)
 mt.__namecall = newcclosure(function(...)
     local args = {...}
     if Enabled and tostring(args[1]) == "SayMessageRequest" then
-        args[2] = bypassText(args[2])
+        args[2] = bypassText(args[2], true)
         return backupnamecall(unpack(args))
     end
     return backupnamecall(unpack(args))
 end)
 setreadonly(mt, true)
+
+print('Initialised Chat Bypass!')
