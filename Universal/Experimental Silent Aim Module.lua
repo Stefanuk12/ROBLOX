6ed5ad8b81399904b540982c8f9461abc58f981c
Stefanuk12 <stefanukpadd@gmail.com>
2020-07-10 17:25:36 +0100

    added more customisablility and examples

--- Universal/Experimental Silent Aim Module.lua
+++ Universal/Experimental Silent Aim Module.lua
@@ -49,68 +49,45 @@ end
 setreadonly(math, false); math.chance = function(percentage) local percentage = math.floor(percentage); local chance = math.floor(Random.new().NextNumber(Random.new(), 0, 1) * 100)/100; return chance <= percentage/100 end; setreadonly(math, true);
 setreadonly(table, false); table.loopforeach = function(tbl, func) for index, value in pairs(tbl) do if type(value) == 'table' then table.loopforeach(value, func); elseif type(value) == 'function' then table.loopforeach(debug.getupvalues(value)); else func(index, value); end; end; end; setreadonly(table, true);
 
--- // Silent Aim Function
-function ValiantAimHacks.getClosestPlayerToCursor()
-    local ClosestPlayer = nil
-    local Chance = math.chance(ValiantAimHacks["HitChance"])
-    local ShortestDistance = math.huge
-    if not Chance then ValiantAimHacks["Selected"] = (Chance and LocalPlayer or LocalPlayer) return (Chance and LocalPlayer or LocalPlayer) end
-    function isPartVisible(Part, PartDescendant)
-        local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-        local Origin = CurrentCamera.CFrame.p
-        local _, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, Part.Position)
-        if OnScreen then
-            local newRay = Ray.new(Origin, Part.Position - Origin)
-            local PartHit, _ = Workspace.FindPartOnRayWithIgnoreList(Workspace, newRay, {Character, CurrentCamera})
-            local Visible = (not PartHit or PartHit.IsDescendantOf(PartHit, PartDescendant))
-            return Visible
-        end
-        return false
+-- // Customisable Checking Functions
+
+function ValiantAimHacks.isPartVisible(Part, PartDescendant)
+    local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
+    local Origin = CurrentCamera.CFrame.p
+    local _, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, Part.Position)
+    if OnScreen then
+        local newRay = Ray.new(Origin, Part.Position - Origin)
+        local PartHit, _ = Workspace.FindPartOnRayWithIgnoreList(Workspace, newRay, {Character, CurrentCamera})
+        local Visible = (not PartHit or PartHit.IsDescendantOf(PartHit, PartDescendant))
+        return Visible
     end
-    function checkTeam(localPlayer, targetPlayer)
-        if targetPlayer.Team ~= localPlayer.Team then
-            for _,v in pairs(ValiantAimHacks.BlacklistedTeams) do
-                if targetPlayer.Team ~= v.Team and targetPlayer.TeamColor ~= v.TeamColor then
-                    return true
-                end
-            end
-        end
-        return false
-    end
-    function checkPlayer(targetPlayer)
-        for i,v in pairs(ValiantAimHacks.BlacklistedPlayers) do
-            if v ~= targetPlayer then
+    return false
+end
+function ValiantAimHacks.checkTeam(localPlayer, targetPlayer)
+    if targetPlayer.Team ~= localPlayer.Team then
+        for _,v in pairs(ValiantAimHacks.BlacklistedTeams) do
+            if targetPlayer.Team ~= v.Team and targetPlayer.TeamColor ~= v.TeamColor then
                 return true
             end
         end
-        return false
     end
-    function checkWhitelisted(targetPlayer)
-        for i,v in pairs(ValiantAimHacks.WhitelistedPUIDs) do
-            if targetPlayer.UserId == v then
-                return true
-            end
-        end
-        return false
-    end
-    for _,plr in pairs(Players.GetPlayers(Players)) do
-        if not checkWhitelisted(plr) and checkPlayer(plr) and plr.Character and plr.Character.PrimaryPart and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid") and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid").Health > 0 then
-            if (ValiantAimHacks["TeamCheck"] and not checkTeam(LocalPlayer, plr)) then break end
-            local PartPos, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, plr.Character.PrimaryPart.Position)
-            local Magnitude = (Vector2.new(PartPos.X, PartPos.Y) - Vector2.new(Mouse.X, Mouse.Y)).magnitude  
-            if (Magnitude < (ValiantAimHacks["FOV"] * 6 - 8)) and (Magnitude < ShortestDistance) then
-                if ValiantAimHacks["VisibleCheck"] and isPartVisible(plr.Character.PrimaryPart, plr.Character) then
-                    ClosestPlayer = plr
-                    ShortestDistance = Magnitude
-                elseif not ValiantAimHacks["VisibleCheck"] then
-                    ClosestPlayer = plr
-                    ShortestDistance = Magnitude
-                end
-            end
+    return false
+end
+function ValiantAimHacks.checkPlayer(targetPlayer)
+    for i,v in pairs(ValiantAimHacks.BlacklistedPlayers) do
+        if v ~= targetPlayer then
+            return true
         end
     end
-    ValiantAimHacks["Selected"] = (Chance and ClosestPlayer or LocalPlayer)
-    return (Chance and ClosestPlayer or LocalPlayer)
+    return false
+end
+function ValiantAimHacks.checkWhitelisted(targetPlayer)
+    for i,v in pairs(ValiantAimHacks.WhitelistedPUIDs) do
+        if targetPlayer.UserId == v then
+            return true
+        end
+    end
+    return false
 end
 
 function ValiantAimHacks.findDirectionNormalMaterial(Origin, Destination, UnitMultiplier)
@@ -134,6 +111,32 @@ function ValiantAimHacks.checkSilentAim()
     return false
 end
 
+-- // Silent Aim Function
+function ValiantAimHacks.getClosestPlayerToCursor()
+    local ClosestPlayer = nil
+    local Chance = math.chance(ValiantAimHacks["HitChance"])
+    local ShortestDistance = math.huge
+    if not Chance then ValiantAimHacks["Selected"] = (Chance and LocalPlayer or LocalPlayer) return (Chance and LocalPlayer or LocalPlayer) end
+    for _,plr in pairs(Players.GetPlayers(Players)) do
+        if not ValiantAimHacks.checkWhitelisted(plr) and ValiantAimHacks.checkPlayer(plr) and plr.Character and plr.Character.PrimaryPart and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid") and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid").Health > 0 then
+            if (ValiantAimHacks["TeamCheck"] and not ValiantAimHacks.checkTeam(LocalPlayer, plr)) then break end
+            local PartPos, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, plr.Character.PrimaryPart.Position)
+            local Magnitude = (Vector2.new(PartPos.X, PartPos.Y) - Vector2.new(Mouse.X, Mouse.Y)).magnitude  
+            if (Magnitude < (ValiantAimHacks["FOV"] * 6 - 8)) and (Magnitude < ShortestDistance) then
+                if ValiantAimHacks["VisibleCheck"] and ValiantAimHacks.isPartVisible(plr.Character.PrimaryPart, plr.Character) then
+                    ClosestPlayer = plr
+                    ShortestDistance = Magnitude
+                elseif not ValiantAimHacks["VisibleCheck"] then
+                    ClosestPlayer = plr
+                    ShortestDistance = Magnitude
+                end
+            end
+        end
+    end
+    ValiantAimHacks["Selected"] = (Chance and ClosestPlayer or LocalPlayer)
+    return (Chance and ClosestPlayer or LocalPlayer)
+end
+
 -- // Heartbeat Function
 local HBFuncs = function()
     ValiantAimHacks.updateCircle()
@@ -142,3 +145,48 @@ end
 Heartbeat.Connect(Heartbeat, HBFuncs)
 --setreadonly(mt, true)
 return ValiantAimHacks
+
+--[[
+Examples:
+
+--// Namecall Version // --
+local mt = getrawmetatable(game)
+local backupindex = mt.__index
+local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
+ValiantAimHacks["TeamCheck"] = false
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if method == "FireServer" then
+        if tostring(args[1]) == "RemoteNameHere" then
+            -- change args
+            return backupnamecall(unpack(args))
+        end
+    end
+    return backupnamecall(...)
+end)
+setreadonly(mt, true)
+
+-- // Index Version // --
+local mt = getrawmetatable(game)
+local backupindex = mt.__index
+local ValiantAimHacks = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Experimental%20Silent%20Aim%20Module.lua"))()
+ValiantAimHacks["TeamCheck"] = false
+setreadonly(mt, false)
+
+mt.__index = newcclosure(function(t, k)
+    if t:IsA("Mouse") and (k == "Hit" or k == "Target") then
+        if ValiantAimHacks.checkSilentAim() then
+            local CPlayer = rawget(ValiantAimHacks, "Selected")
+            if CPlayer and CPlayer.Character and CPlayer.Character.FindFirstChild(CPlayer.Character, "Head") then
+                return (k == "Hit" and CPlayer.Character.Head.CFrame or CPlayer.Character.Head)
+            end
+        end
+    end
+    return backupindex(t, k)
+end)
+setreadonly(mt, true)
+
+]]
