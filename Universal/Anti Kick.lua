22d4c02d9e533332f94a9e4e61cf21ff522700d2
Stefanuk12 <stefanukpadd@gmail.com>
2020-07-15 01:35:21 +0100

    remove unused vars

--- Universal/Anti Kick.lua
+++ Universal/Anti Kick.lua
@@ -1,20 +1,9 @@
--- // Valiant ENV
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
-
 -- // Vars
-local RenderStepped = RunService.RenderStepped
-local Heartbeat = RunService.Heartbeat
-local LocalPlayer = Players.LocalPlayer
-local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-local Humanoid = Character.WaitForChild(Character, "Humanoid")
-local CurrentCamera = Workspace.CurrentCamera
-local Mouse = LocalPlayer.GetMouse(LocalPlayer)
+local LocalPlayer = game:GetService("Players").LocalPlayer
 
 -- // Base MT Vars + Funs
 local mt = getrawmetatable(game)
 local backupnamecall = mt.__namecall
-local backupnewindex = mt.__newindex
-local backupindex = mt.__index 
 setreadonly(mt, false)
 
 -- // Anti kick
