0d90e4abdeeea702ace6b350042dfd7b815a3e90
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-08-25 12:55:09 +0100

    update

--- Games/Kohls Admin House/Main.lua
+++ Games/Kohls Admin House/Main.lua
@@ -872,7 +872,7 @@ addCMD("whitelist", "Control", "whitelist EpicGamer69", "Whitelists the player t
                 vars.PlayerManager[v.Name]["Whitelisted"] = true
                 vars.Notify("Whitelisted "..v.Name.."!")
                 Players:Chat(":pm " .. v.Name .. " You have been whitelisted to use oof kohls created by Stefanuk12, follow the link in logs for further info.")
-                Players:Chat(":pm me bit.ly/wlcmds")
+                Players:Chat(":pm __ bit.ly/wlcmds")
             end
         end
         vars.Notify("Please redirect whitelisted users to: bit.ly/wlcmds. This link redirects to all of the commands they have access to and any extra info they may need.")
