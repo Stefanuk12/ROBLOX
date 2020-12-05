e5a1205c39ca79d070067dcd4095cc8522988afb
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-12-05 21:28:28 +0000

    Update

--- Universal/Music API/JavaScript Version/Examples/AddSounds.js
+++ Universal/Music API/JavaScript Version/Examples/AddSounds.js
@@ -5,7 +5,23 @@ const config = require("../config.json");
 
 // Vars
 const soundsToAdd = [
-    {Name: "", SoundId: ""}
+    {Name: "XXXTentacion - going down!", SoundId: "6031259337"},
+    {Name: "$NOT - Mean", SoundId: "6033522087"},
+    {Name: "Lil Tecca - Love No Thot", SoundId: "6034017442"},
+    {Name: "Lil Tecca - Amigo", SoundId: "6029385077"},
+    {Name: "YNW Melly - Suicidal", SoundId: "6029387414"},
+    {Name: "Lil Nas X - HOLIDAY", SoundId: "6029390564"},
+    {Name: "XXXTENTACION - King Of The Dead", SoundId: "5285302193"},
+    {Name: "POP SMOKE - ELEMENT", SoundId: "6009015262"},
+    {Name: "Lil Peep - Paid 2 Party", SoundId: "5957778837"},
+    {Name: "XXXTENTACION - ILOVEITWHENTHEYRUN", SoundId: "5142292418"},
+    {Name: "Juice WRLD - Tell Me U Luv Me ft. Trippie Redd", SoundId: "5155864261"},
+    {Name: "$UICIDEBOY$ - NOW I’M UP TO MY NECK WITH OFFERS", SoundId: "5156522678"},
+    {Name: "Lil Tracy x Lil Peep - GODS", SoundId: "5278542681"},
+    {Name: "Lil Tecca - Out Of Love", SoundId: "5285672433"},
+    {Name: "$uicideboy$ - Kill Yourself Part III", SoundId: "5121386882"},
+    {Name: "$UICIDEBOY$ x POUYA - RUNNIN' THRU THE 7TH WITH MY WOADIES", SoundId: "5278241371"},
+    {Name: "$UICIDEBOY$ x BLACK SMURF - ...AND SO IT WAS", SoundId: "5125677449"}
 ];
 
 // 
@@ -17,7 +33,7 @@ fs.readFile(`${config.MusicTableLocation}/${config.MusicTableName}`, "utf8", asy
     var MusicTable = JSON.parse(data);
     var newMusicTable = await MusicAPI.addSoundsToMusicTable(soundsToAdd, MusicTable);
 
-    fs.writeFile(`${config.MusicTableLocation}/${config.MusicTableName}`, newMusicTable, "utf8", function(err, data){
+    fs.writeFile(`${config.MusicTableLocation}/${config.MusicTableName}`, JSON.stringify(newMusicTable), "utf8", function(err, data){
         if (err){
             throw(err);
         };
