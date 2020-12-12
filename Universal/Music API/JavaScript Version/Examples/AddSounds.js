bd9334310d3a1dae79e384778fd3d6046eabb07f
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-12-12 22:23:04 +0000

    More Sounds

--- Universal/Music API/JavaScript Version/Examples/AddSounds.js
+++ Universal/Music API/JavaScript Version/Examples/AddSounds.js
@@ -5,23 +5,15 @@ const config = require("../config.json");
 
 // Vars
 const soundsToAdd = [
-    {Name: "XXXTentacion - going down!", SoundId: "6031259337"},
-    {Name: "$NOT - Mean", SoundId: "6033522087"},
-    {Name: "Lil Tecca - Love No Thot", SoundId: "6034017442"},
-    {Name: "Lil Tecca - Amigo", SoundId: "6029385077"},
-    {Name: "YNW Melly - Suicidal", SoundId: "6029387414"},
-    {Name: "Lil Nas X - HOLIDAY", SoundId: "6029390564"},
-    {Name: "XXXTENTACION - King Of The Dead", SoundId: "5285302193"},
-    {Name: "POP SMOKE - ELEMENT", SoundId: "6009015262"},
-    {Name: "Lil Peep - Paid 2 Party", SoundId: "5957778837"},
-    {Name: "XXXTENTACION - ILOVEITWHENTHEYRUN", SoundId: "5142292418"},
-    {Name: "Juice WRLD - Tell Me U Luv Me ft. Trippie Redd", SoundId: "5155864261"},
-    {Name: "$UICIDEBOY$ - NOW I’M UP TO MY NECK WITH OFFERS", SoundId: "5156522678"},
-    {Name: "Lil Tracy x Lil Peep - GODS", SoundId: "5278542681"},
-    {Name: "Lil Tecca - Out Of Love", SoundId: "5285672433"},
-    {Name: "$uicideboy$ - Kill Yourself Part III", SoundId: "5121386882"},
-    {Name: "$UICIDEBOY$ x POUYA - RUNNIN' THRU THE 7TH WITH MY WOADIES", SoundId: "5278241371"},
-    {Name: "$UICIDEBOY$ x BLACK SMURF - ...AND SO IT WAS", SoundId: "5125677449"}
+    {Name: "24kGoldn - Coco", SoundId: "6070511325"},
+    {Name: "Mariah Carey - All I Want For Christmas Is You", SoundId: "6066714802"},
+    {Name: "Dapz - Mama", SoundId: "6070410633"},
+    {Name: "JAY1 - Your Mrs", SoundId: "6070451257"},
+    {Name: "JAY1 - GWOP", SoundId: "6070455638"},
+    {Name: "JAY1 - Good Vibes ", SoundId: "6066403641"},
+    {Name: "JAY1 - Sweet One", SoundId: "6066599352"},
+    {Name: "Dapz - Take You Away", SoundId: "6066639778"},
+    {Name: "The Pogues - Fairytale Of New York", SoundId: "6066748416"}
 ];
 
 // 
