5e4b7535190d4ee73b6886cbeb53009fc2a3a479
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2020-12-25 21:20:01 +0000

    Enchancements

--- Universal/Experimental Silent Aim Module.lua
+++ Universal/Experimental Silent Aim Module.lua
@@ -24,14 +24,15 @@ getgenv().ValiantAimHacks = {
             TeamColor = LocalPlayer.TeamColor,
         },
     },
-    BlacklistedPlayers = {game.GetService(game, "Players").LocalPlayer},
+    BlacklistedPlayers = {LocalPlayer},
     WhitelistedPUIDs = {91318356},
 }
 
 -- // Show FOV
 local circle = Drawing.new("Circle")
 function ValiantAimHacks.updateCircle()
-    if circle then
+    if (circle) then
+        -- // Set Circe Properties
         circle.Transparency = 1
         circle.Visible = ValiantAimHacks["ShowFOV"]
         circle.Thickness = 2
@@ -40,6 +41,8 @@ function ValiantAimHacks.updateCircle()
         circle.Radius = (ValiantAimHacks["FOV"] * 6) / 2
         circle.Filled = false
         circle.Position = Vector2.new(Mouse.X, Mouse.Y + (GuiService.GetGuiInset(GuiService).Y))
+
+        -- // Return circle
         return circle
     end
 end
@@ -48,93 +51,141 @@ end
 setreadonly(math, false); math.chance = function(percentage) local percentage = math.floor(percentage); local chance = math.floor(Random.new().NextNumber(Random.new(), 0, 1) * 100)/100; return chance <= percentage/100 end; setreadonly(math, true);
 setreadonly(table, false); table.loopforeach = function(tbl, func) for index, value in pairs(tbl) do if type(value) == 'table' then table.loopforeach(value, func); elseif type(value) == 'function' then table.loopforeach(debug.getupvalues(value)); else func(index, value); end; end; end; setreadonly(table, true);
 
--- // Customisable Checking Functions
-
+-- // Customisable Checking Functions: Is a part visible
 function ValiantAimHacks.isPartVisible(Part, PartDescendant)
+    -- // Vars
     local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-    local Origin = CurrentCamera.CFrame.p
+    local Origin = CurrentCamera.CFrame.Position
     local _, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, Part.Position)
-    if OnScreen then
+
+    -- // If Part is on the screen
+    if (OnScreen) then
+        -- // Vars: Calculating if is visible
         local newRay = Ray.new(Origin, Part.Position - Origin)
         local PartHit, _ = Workspace.FindPartOnRayWithIgnoreList(Workspace, newRay, {Character, CurrentCamera})
         local Visible = (not PartHit or PartHit.IsDescendantOf(PartHit, PartDescendant))
+
+        -- // Return
         return Visible
     end
+
+    -- // Return
     return false
 end
 
+-- // Check teams
 function ValiantAimHacks.checkTeam(targetPlayer)
-    if targetPlayer.Team ~= LocalPlayer.Team then
-        for _,v in pairs(ValiantAimHacks.BlacklistedTeams) do
-            if targetPlayer.Team ~= v.Team and targetPlayer.TeamColor ~= v.TeamColor then
+    -- // If player is not on your team
+    if (targetPlayer.Team ~= LocalPlayer.Team) then
+
+        -- // Check if team is blacklisted
+        for i = 1, #ValiantAimHacks.BlacklistedTeams do
+            local v = ValiantAimHacks.BlacklistedTeams
+
+            if (targetPlayer.Team ~= v.Team and targetPlayer.TeamColor ~= v.TeamColor) then
                 return true
             end
         end
     end
+
+    -- // Return
     return false
 end
 
+-- // Check if player is blacklisted
 function ValiantAimHacks.checkPlayer(targetPlayer)
-    for i,v in pairs(ValiantAimHacks.BlacklistedPlayers) do
-        if v ~= targetPlayer then
+    for i = 1, #ValiantAimHacks.BlacklistedPlayers do
+        local v = ValiantAimHacks.BlacklistedPlayers[i]
+
+        if (v ~= targetPlayer) then
             return true
         end
     end
+
+    -- // Return
     return false
 end
 
+-- // Check if player is whitelisted
 function ValiantAimHacks.checkWhitelisted(targetPlayer)
-    for i,v in pairs(ValiantAimHacks.WhitelistedPUIDs) do
-        if targetPlayer.UserId == v then
+    for i = 1, #ValiantAimHacks.WhitelistedPUIDs do
+        local v = ValiantAimHacks.WhitelistedPUIDs[i]
+
+        if (targetPlayer.UserId == v) then
             return true
         end
     end
+
+    -- // Return
     return false
 end
 
+-- // Get the Direction, Normal and Material
 function ValiantAimHacks.findDirectionNormalMaterial(Origin, Destination, UnitMultiplier)
-    if typeof(Origin) == "Vector3" and typeof(Destination) == "Vector3" then
-        if not UnitMultiplier then UnitMultiplier = 1 end
+    if (typeof(Origin) == "Vector3" and typeof(Destination) == "Vector3") then
+        -- // Handling
+        if (not UnitMultiplier) then UnitMultiplier = 1 end
+
+        -- // Vars
         local Direction = (Destination - Origin).Unit * UnitMultiplier
         local RaycastResult = Workspace.Raycast(Workspace, Origin, Direction)
-        if RaycastResult ~= nil then
+
+        if (RaycastResult ~= nil) then
             local Normal = RaycastResult.Normal
             local Material = RaycastResult.Material
+
             return Direction, Normal, Material
         end
     end
+
+    -- // Return
     return nil
 end
 
+-- // Check if silent aim can used
 function ValiantAimHacks.checkSilentAim()
-    if rawget(ValiantAimHacks, "SilentAimEnabled") == true and rawget(ValiantAimHacks, "Selected") ~= LocalPlayer then
-        return true
-    end
-    return false
+    return (rawget(ValiantAimHacks, "SilentAimEnabled") == true and rawget(ValiantAimHacks, "Selected") ~= LocalPlayer)
 end
 
 -- // Silent Aim Function
 function ValiantAimHacks.getClosestPlayerToCursor()
+    -- // Vars
     local ClosestPlayer = nil
     local Chance = math.chance(ValiantAimHacks["HitChance"])
     local ShortestDistance = 1/0
-    if not Chance then ValiantAimHacks["Selected"] = (Chance and LocalPlayer or LocalPlayer) return (Chance and LocalPlayer or LocalPlayer) end
-    for _,plr in pairs(Players.GetPlayers(Players)) do
-        if not ValiantAimHacks.checkWhitelisted(plr) and ValiantAimHacks.checkPlayer(plr) and plr.Character and plr.Character.PrimaryPart and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid") and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid").Health > 0 then
+
+    -- // Chance
+    if (not Chance) then 
+        ValiantAimHacks["Selected"] = (Chance and LocalPlayer or LocalPlayer)
+        
+        return (Chance and LocalPlayer or LocalPlayer)
+    end
+
+    -- // Loop through all players
+    local AllPlayers = Players.GetPlayers(Players)
+    for i = 1, #AllPlayers do
+        local plr = AllPlayers[i]
+
+        if (not ValiantAimHacks.checkWhitelisted(plr) and ValiantAimHacks.checkPlayer(plr) and plr.Character and plr.Character.PrimaryPart and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid") and plr.Character.FindFirstChildWhichIsA(plr.Character, "Humanoid").Health > 0) then
+            -- // Team Check
             if (ValiantAimHacks["TeamCheck"] and not ValiantAimHacks.checkTeam(LocalPlayer, plr)) then break end
+
+            -- // Vars
             local PartPos, OnScreen = CurrentCamera.WorldToViewportPoint(CurrentCamera, plr.Character.PrimaryPart.Position)
-            local Magnitude = (Vector2.new(PartPos.X, PartPos.Y) - Vector2.new(Mouse.X, Mouse.Y)).magnitude  
+            local Magnitude = (Vector2.new(PartPos.X, PartPos.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
+
+            -- // Check if is in FOV
             if (Magnitude < (ValiantAimHacks["FOV"] * 6 - 8)) and (Magnitude < ShortestDistance) then
-                if ValiantAimHacks["VisibleCheck"] and ValiantAimHacks.isPartVisible(plr.Character.PrimaryPart, plr.Character) then
-                    ClosestPlayer = plr
-                    ShortestDistance = Magnitude
-                elseif not ValiantAimHacks["VisibleCheck"] then
+                -- // Check if Visible
+                if (ValiantAimHacks["VisibleCheck"] and ValiantAimHacks.isPartVisible(plr.Character.PrimaryPart, plr.Character)) or (not ValiantAimHacks["VisibleCheck"]) then
                     ClosestPlayer = plr
                     ShortestDistance = Magnitude
                 end
             end
         end
     end
+
+    -- // End
     ValiantAimHacks["Selected"] = (Chance and ClosestPlayer or LocalPlayer)
     return (Chance and ClosestPlayer or LocalPlayer)
 end
