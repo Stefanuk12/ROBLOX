c65ed0a7f445cf635176a147c2308ca3ced29cd3
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-01-17 23:58:14 +0000

    Done

--- Games/D-Day/AC Bypass.lua
+++ Games/D-Day/AC Bypass.lua
@@ -1,133 +1,121 @@
--- Game: https://roblox.com/games/5061412825
-
--- // Valiant ENV
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ValiantENV.lua"))()
+-- // Services
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+local Lighting = game:GetService("Lighting")
 
 -- // Vars
-local RenderStepped = RunService.RenderStepped
-local Heartbeat = RunService.Heartbeat
 local LocalPlayer = Players.LocalPlayer
-local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded.Wait(LocalPlayer.CharacterAdded)
-local Humanoid = Character.WaitForChild(Character, "Humanoid")
-local HumanoidRootPart = Character.WaitForChild(Character, "HumanoidRootPart")
-local CurrentCamera = Workspace.CurrentCamera
-local Mouse = LocalPlayer.GetMouse(LocalPlayer)
-local Weapons = ReplicatedStorage.Weapons
-local ClientBullet = ReplicatedStorage.Modules.Client["Client_Bullet"]
-local Map = Workspace.Map
-local Vegetation = Workspace.Vegetation
+local PlayerGui = LocalPlayer.PlayerGui
+local Map = Workspace:WaitForChild("Map")
+local MapBoundaries = Workspace:WaitForChild("Map_Boundaries")
+local Vegetation = Workspace:WaitForChild("Vegetation")
 
-getgenv().DDayHax = {}
-DDayHax.SpoofedValues = {
-    ["WalkSpeed"] = {
-		["Default"] = 16
-	},
-    ["JumpPower"] = {
-		["Default"] = 50
-	},
-}
-
-DDayHax.Connections = {
-    LocalPlayer.PlayerScripts["Client_Second"].AncestryChanged,
-    LocalPlayer.PlayerScripts["Client_Second"].Changed,
-    Map.AncestryChanged,
-    Vegetation.AncestryChanged,
-}
-
-
--- // Base MT Vars + Funs
-local mt = getrawmetatable(game)
-local backupnamecall = mt.__namecall
-local backupnewindex = mt.__newindex
-local backupindex = mt.__index 
-setreadonly(mt, false)
-
-function disableACPart1()
-    function disableConnections(connection)
-        for _,v in pairs(getconnections(connection)) do
-            v:Disable()
+-- // Remove detection functions
+do
+    local gGC = getgc()
+    for i = 1, #gGC do
+        local v = gGC[i]
+        local functionName = debug.getinfo(v).name
+        if (functionName == "AnotherLayer" or functionName == "DetectBodyStuff") then
+            hookfunction(v, function() end)
         end
     end
+end
 
-    -- // Disable AC Connections
-    for i,v in pairs(Weapons:GetChildren()) do
-        disableConnections(v.ChildAdded)
+-- // Hook wait t
+local oldWait
+oldWait = hookfunction(getrenv().wait, function(waitTime)
+    if (waitTime == 3 and getcallingscript().Name == "_Client_Main_") then
+        return false
+    end
 
-        for _,x in pairs(v:GetChildren()) do
-            if x:IsA("ModuleScript") then
-                disableConnections(v.AncestryChanged)
+    return oldWait(waitTime)
+end)
+
+-- // Remove connections
+local baseConnections = {
+    PlayerGui.ChildAdded,
+    Workspace.CurrentCamera.ChildAdded,
+    Map.AncestryChanged,
+    Vegetation.AncestryChanged,
+    MapBoundaries.AncestryChanged,
+    Lighting.Changed
+}
+
+for i = 1, #baseConnections do
+    local connections = getconnections(baseConnections[i])
+    for i = 1, #connections do
+        local connection = connections[i]
+        if (getfenv(connection.Function).script.Name == "_Client_Main_") then
+            connection:Disable()
+        end
+    end
+end
+
+do
+    local mapChildren = Map:GetChildren()
+    for i = 1, #mapChildren do
+        local child = mapChildren[i]
+        if (child.Name == "Sandbags") then
+            -- // Remove sandbag connections
+            local connections = getconnections(child.AncestryChanged)
+            for i = 1, #connections do
+                local connection = connections[i]
+                if (getfenv(connection.Function).script.Name == "_Client_Main_") then
+                    connection:Disable()
+                end
+            end
+
+            -- // Remove sandbag children connections
+            local sandbagChildren = child:GetChildren()
+            for i = 1, #sandbagChildren do
+                local child = sandbagChildren[i]
+                if (child:IsA("BasePart")) then
+
+                    -- // AncestryChanged
+                    local connections = getconnections(child.AncestryChanged)
+                    for i = 1, #connections do
+                        local connection = connections[i]
+                        if (getfenv(connection.Function).script.Name == "_Client_Main_") then
+                            connection:Disable()
+                        end
+                    end
+
+                    -- // Changed
+                    local connections = getconnections(child.Changed)
+                    for i = 1, #connections do
+                        local connection = connections[i]
+                        if (getfenv(connection.Function).script.Name == "_Client_Main_") then
+                            connection:Disable()
+                        end
+                    end
+                end
             end
         end
     end
+end
 
-    for i,v in pairs(DDayHax.Connections) do
-        disableConnections(v)
-    end
+do
+    local vegetationChildren = Vegetation:GetChildren()
+    for i = 1, #vegetationChildren do
+        local child = vegetationChildren[i]
 
-    for _,connection in pairs(getconnections(LocalPlayer.PlayerGui.ChildAdded)) do
-        for i,v in pairs(debug.getconstants(connection.Function)) do
-            if tostring(v) == 'DDAY' then
+        -- // AncestryChanged
+        local connections = getconnections(child.AncestryChanged)
+        for i = 1, #connections do
+            local connection = connections[i]
+            if (getfenv(connection.Function).script.Name == "_Client_Main_") then
+                connection:Disable()
+            end
+        end
+
+        -- // Changed
+        local connections = getconnections(child.Changed)
+        for i = 1, #connections do
+            local connection = connections[i]
+            if (getfenv(connection.Function).script.Name == "_Client_Main_") then
                 connection:Disable()
             end
         end
     end
-
-    for i,v in pairs(Map:GetChildren()) do
-        if v.Name == "Sandbags" then
-            disableConnections(v.AncestryChanged)
-        end
-        for a,x in pairs(v:GetChildren()) do
-            if x:IsA("BasePart") then
-                disableConnections(x.AncestryChanged)
-                disableConnections(x.Changed)
-            end
-        end
-    end
-
-    for i,v in pairs(Vegetation:GetChildren()) do
-        disableConnections(v.AncestryChanged)
-    end
-
-    -- // Disable Functions
-    for i,v in ipairs(getgc()) do
-        if debug.getinfo(v).name == "DetectBodyStuff" or debug.getinfo(v).name == "AnotherLayer" then
-            hookfunction(v, function() return end)
-        end
-
-        if debug.getfenv(v).script ~= nil and debug.getfenv(v).script.Name == "_Client_Main_" then 
-            disableConnections(getfenv(v).script.AncestryChanged)
-        end
-    end
-end
-disableACPart1()
-Character.CharacterAdded:Connect(disableACPart1)
-
--- // MT Stuff
-hookfunction(LocalPlayer.Kick, warn)
-hookfunction(LocalPlayer.kick, warn)
-if not DDayHax.Part2 then
-    mt.__index = newcclosure(function(t, k)
-        if t == HumanoidRootPart and k == "Anchored" then
-            return false
-        elseif t == Humanoid and k == "PlatformStand" then
-            return false
-        end
-        if DDayHax.SpoofedValues[k] then
-            return DDayHax.SpoofedValues[k]["Default"]
-        end
-
-        return backupindex(t, k)
-    end)
-
-    mt.__namecall = newcclosure(function(...)
-        local method = getnamecallmethod()
-        if method == 'GetRealPhysicsFPS' then
-            return 71
-        end
-        if string.lower(method) == "kick" then
-            return nil
-        end
-        return backupnamecall(...)
-    end)
-    DDayHax.Part2 = true
 end
