4668f0f29c60078613f2ec3b31a981d47da604a6
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-03-14 18:24:57 +0000

    Stability improvements

--- Games/Build A Boat For Treasure/Autofarm.lua
+++ Games/Build A Boat For Treasure/Autofarm.lua
@@ -1,8 +1,7 @@
 -- // Custom Settings
 getgenv().TreasureAutoFarm = {
     Enabled = true, -- // Toggle the auto farm on and off
-    Teleport = 2, -- // How fast between each teleport between the stages and stuff
-    ClaimGold = 2, -- // How long to wait until it claims the gold after going through all of the stages
+    Teleport = 2 -- // How fast between each teleport between the stages and stuff
 }
 
 -- // Services
@@ -12,31 +11,16 @@ local Lighting = game:GetService("Lighting")
 
 -- // Vars
 local LocalPlayer = Players.LocalPlayer
-local InstaLoadFunction = Workspace.InstaLoadFunction
 
--- // Claim money
-local ClaimFunction = function()
+-- // Goes through all of the stages
+local autoFarm = function(currentRun)
+    -- // Claim gold (if you have any to get)
     local RiverResultsGui = LocalPlayer.PlayerGui:FindFirstChild("RiverResultsGui")
-    
+
     if (RiverResultsGui) then
         local ClaimButton = RiverResultsGui.Frame.BuyButton
         firesignal(ClaimButton.MouseButton1Click)
     end
-end
-
--- // Create a temp part under you
-local createTempPart = function()
-    local Part = Instance.new("Part", LocalPlayer.Character)
-    Part.Anchored = true
-    Part.Position = LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0, 3, 0)
-
-    return Part
-end
-
--- // Goes through all of the stages
-local walkthrough = function()
-    -- // Claim gold (if you have any to get)
-    ClaimFunction()
 
     -- // Variables
     local Character = LocalPlayer.Character
@@ -44,16 +28,21 @@ local walkthrough = function()
 
     -- // Go to each stage thing
     do
-        local Stages = NormalStages:GetChildren()
-        for i = 1, #Stages do
-            local Stage = Stages[i]
+        for i = 1, 10 do
+            local Stage = NormalStages["CaveStage" .. i]
             local DarknessPart = Stage:FindFirstChild("DarknessPart")
 
             if (DarknessPart) then
+                -- // Teleport to next stage
+                print("Teleporting to next stage: Stage " .. i)
                 Character.HumanoidRootPart.CFrame = DarknessPart.CFrame
 
-                local Part = createTempPart()
-                Character:WaitForChild("Humanoid").Jump = true -- // Jump to make sure it registers
+                -- // Create a temp part under you
+                local Part = Instance.new("Part", LocalPlayer.Character)
+                Part.Anchored = true
+                Part.Position = LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0, 6, 0)
+
+                -- // Wait and remove temp part
                 wait(getgenv().TreasureAutoFarm.Teleport)
                 Part:Destroy()
             end
@@ -61,22 +50,29 @@ local walkthrough = function()
     end
 
     -- // Go to end
+    print("Teleporting to the end")
     repeat wait()
         Character.HumanoidRootPart.CFrame = NormalStages.TheEnd.GoldenChest.Trigger.CFrame
     until Lighting.ClockTime ~= 14
 
-    -- // Instant Die
-    wait(getgenv().TreasureAutoFarm.ClaimGold)
-    InstaLoadFunction:InvokeServer(nil, true)
-    LocalPlayer.CharacterAdded:Wait()
+    -- // Wait until you have respawned
+    local Respawned = false
+    local Connection
+    Connection = LocalPlayer.CharacterAdded:Connect(function()
+        Respawned = true
+        Connection:Disconnect()
+    end)
 
-    -- // Claim gold (if you have any to get)
-    ClaimFunction()
+    repeat wait() until Respawned
+    print("Auto Farm: Run " .. currentRun .. " finished")
 end
 
 -- // Whilst the autofarm is enable, constantly do it
+local autoFarmRun = 1
 while wait() do
     if (getgenv().TreasureAutoFarm.Enabled) then
-        walkthrough()
+        print("Initialising Auto Farm: Run " .. autoFarmRun)
+        autoFarm(autoFarmRun)
+        autoFarmRun = autoFarmRun + 1
     end
 end
