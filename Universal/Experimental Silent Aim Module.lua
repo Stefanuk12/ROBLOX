9e77b5faa10389d1373be92154b9b49c98113401
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-03-18 21:33:31 +0000

    Added ability for multiple target parts and optimisations.

--- Universal/Experimental Silent Aim Module.lua
+++ Universal/Experimental Silent Aim Module.lua
@@ -12,6 +12,24 @@ local LocalPlayer = Players.LocalPlayer
 local CurrentCamera = Workspace.CurrentCamera
 local Mouse = LocalPlayer:GetMouse()
 
+-- // Optimisation Vars (ugly)
+local Drawingnew = Drawing.new
+local Color3fromRGB = Color3.fromRGB
+local Vector2new = Vector2.new
+local GetGuiInset = GuiService.GetGuiInset
+local Randomnew = Random.new
+local mathfloor = math.floor
+local CharacterAddedWait = LocalPlayer.CharacterAdded.Wait
+local WorldToViewportPoint = CurrentCamera.WorldToViewportPoint
+local RaycastParamsnew = RaycastParams.new
+local EnumRaycastFilterTypeBlacklist = Enum.RaycastFilterType.Blacklist
+local Raycast = Workspace.Raycast
+local GetPlayers = Players.GetPlayers
+local Instancenew = Instance.new
+local IsDescendantOf = Instancenew("Part").IsDescendantOf
+local FindFirstChildWhichIsA = Instancenew("Part").FindFirstChildWhichIsA
+local FindFirstChild = Instancenew("Part").FindFirstChild
+
 -- // Silent Aim Vars
 getgenv().ValiantAimHacks = {
     SilentAimEnabled = true,
@@ -21,7 +39,8 @@ getgenv().ValiantAimHacks = {
     FOV = 60,
     HitChance = 100,
     Selected = LocalPlayer,
-    TargetPart = "Head",
+    SelectedPart = nil,
+    TargetPart = {"Head", "HumanoidRootPart"},
     BlacklistedTeams = {
         {
             Team = LocalPlayer.Team,
@@ -31,12 +50,13 @@ getgenv().ValiantAimHacks = {
     BlacklistedPlayers = {LocalPlayer},
     WhitelistedPUIDs = {91318356},
 }
+local ValiantAimHacks = getgenv().ValiantAimHacks
 
 -- // Show FOV
-local circle = Drawing.new("Circle")
+local circle = Drawingnew("Circle")
 circle.Transparency = 1
 circle.Thickness = 2
-circle.Color = Color3.fromRGB(231, 84, 128)
+circle.Color = Color3fromRGB(231, 84, 128)
 circle.NumSides = 12
 circle.Filled = false
 function ValiantAimHacks.updateCircle()
@@ -44,7 +64,7 @@ function ValiantAimHacks.updateCircle()
         -- // Set Circle Properties
         circle.Visible = ValiantAimHacks.ShowFOV
         circle.Radius = (ValiantAimHacks.FOV * 3)
-        circle.Position = Vector2.new(Mouse.X, Mouse.Y + GuiService:GetGuiInset().Y)
+        circle.Position = Vector2new(Mouse.X, Mouse.Y + GetGuiInset(GuiService).Y)
 
         -- // Return circle
         return circle
@@ -52,29 +72,29 @@ function ValiantAimHacks.updateCircle()
 end
 
 -- // Custom Functions
-calcChance = function(percentage)
-    percentage = math.floor(percentage)
-    local chance = math.floor(Random.new().NextNumber(Random.new(), 0, 1) * 100) / 100
-    return chance <= percentage/100
+local calcChance = function(percentage)
+    percentage = mathfloor(percentage)
+    local chance = mathfloor(Randomnew().NextNumber(Randomnew(), 0, 1) * 100) / 100
+    return chance <= percentage / 100
 end
 
 -- // Customisable Checking Functions: Is a part visible
 function ValiantAimHacks.isPartVisible(Part, PartDescendant)
     -- // Vars
-    local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+    local Character = LocalPlayer.Character or CharacterAddedWait(LocalPlayer)
     local Origin = CurrentCamera.CFrame.Position
-    local _, OnScreen = CurrentCamera:WorldToViewportPoint(Part.Position)
+    local _, OnScreen = WorldToViewportPoint(CurrentCamera, Part.Position)
 
     -- // If Part is on the screen
     if (OnScreen) then
         -- // Vars: Calculating if is visible
-        local raycastParams = RaycastParams.new()
-        raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+        local raycastParams = RaycastParamsnew()
+        raycastParams.FilterType = EnumRaycastFilterTypeBlacklist
         raycastParams.FilterDescendantsInstances = {Character, CurrentCamera}
 
-        local Result = Workspace:Raycast(Origin, Part.Position - Origin, raycastParams)
+        local Result = Raycast(Workspace, Origin, Part.Position - Origin, raycastParams)
         local PartHit = Result.Instance
-        local Visible = (not PartHit or PartHit:IsDescendantOf(PartDescendant))
+        local Visible = (not PartHit or IsDescendantOf(PartHit, PartDescendant))
 
         -- // Return
         return Visible
@@ -139,7 +159,7 @@ function ValiantAimHacks.findDirectionNormalMaterial(Origin, Destination, UnitMu
 
         -- // Vars
         local Direction = (Destination - Origin).Unit * UnitMultiplier
-        local RaycastResult = Workspace:Raycast(Origin, Direction)
+        local RaycastResult = Raycast(Workspace, Origin, Direction)
 
         if (RaycastResult ~= nil) then
             local Normal = RaycastResult.Normal
@@ -161,7 +181,7 @@ end
 -- // Check Health
 function ValiantAimHacks.checkHealth(Player)
     local Character = ValiantAimHacks.getCharacter(Player)
-    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+    local Humanoid = FindFirstChildWhichIsA(Character, "Humanoid")
 
     local Health = (Humanoid and Humanoid.Health or 0)
     return Health > 0
@@ -169,12 +189,46 @@ end
 
 -- // Check if silent aim can used
 function ValiantAimHacks.checkSilentAim()
-    return (ValiantAimHacks.SilentAimEnabled == true and ValiantAimHacks.Selected ~= LocalPlayer)
+    return (ValiantAimHacks.SilentAimEnabled == true and ValiantAimHacks.Selected ~= LocalPlayer and ValiantAimHacks.SelectedPart ~= nil)
+end
+
+-- // Get Closest Target Part
+function ValiantAimHacks.getClosestTargetPartToCursor(Character)
+    local TargetParts = ValiantAimHacks.TargetPart
+
+    -- // String check
+    if (typeof(TargetParts) == "string") then
+        return FindFirstChild(Character, TargetParts)
+    end
+
+    -- // Vars
+    local ClosestPart = nil
+    local ShortestDistance = 1/0
+
+    -- // Loop through all target parts
+    for i = 1, #TargetParts do
+        local TargetPartName = TargetParts[i]
+        local TargetPart = FindFirstChild(Character, TargetPartName)
+
+        if (TargetPart) then
+            local PartPos, _ = WorldToViewportPoint(CurrentCamera, TargetPart.Position)
+            local Magnitude = (Vector2new(PartPos.X, PartPos.Y) - Vector2new(Mouse.X, Mouse.Y)).Magnitude
+
+            if (Magnitude < ShortestDistance) then
+                ClosestPart = TargetPart
+                ShortestDistance = Magnitude
+            end
+        end
+    end
+
+    -- //
+    return ClosestPart
 end
 
 -- // Silent Aim Function
 function ValiantAimHacks.getClosestPlayerToCursor()
     -- // Vars
+    local TargetPart = nil
     local ClosestPlayer = nil
     local Chance = calcChance(ValiantAimHacks.HitChance)
     local ShortestDistance = 1/0
@@ -187,34 +241,38 @@ function ValiantAimHacks.getClosestPlayerToCursor()
     end
 
     -- // Loop through all players
-    local AllPlayers = Players:GetPlayers()
+    local AllPlayers = GetPlayers(Players)
     for i = 1, #AllPlayers do
         local Player = AllPlayers[i]
         local Character = ValiantAimHacks.getCharacter(Player)
 
-        if (not ValiantAimHacks.checkWhitelisted(Player) and ValiantAimHacks.checkPlayer(Player) and Character and Character:FindFirstChild(ValiantAimHacks.TargetPart) and ValiantAimHacks.checkHealth(Player)) then
-            -- // Team Check
-            if (ValiantAimHacks.TeamCheck and not ValiantAimHacks.checkTeam(Player, LocalPlayer)) then continue end
+        if (not ValiantAimHacks.checkWhitelisted(Player) and ValiantAimHacks.checkPlayer(Player) and Character) then
+            TargetPart = ValiantAimHacks.getClosestTargetPartToCursor(Character)
 
-            -- // Vars
-            local TargetPart = Character[ValiantAimHacks.TargetPart]
-            local PartPos, _ = CurrentCamera:WorldToViewportPoint(TargetPart.Position)
-            local Magnitude = (Vector2.new(PartPos.X, PartPos.Y) - Vector2.new(Mouse.X, Mouse.Y)).Magnitude
+            if (TargetPart and ValiantAimHacks.checkHealth(Player)) then
+                -- // Team Check
+                if (ValiantAimHacks.TeamCheck and not ValiantAimHacks.checkTeam(Player, LocalPlayer)) then continue end
 
-            -- // Check if is in FOV
-            if (circle.Radius > Magnitude and Magnitude < ShortestDistance) then
-                -- // Check if Visible
-                if (ValiantAimHacks.VisibleCheck and not ValiantAimHacks.isPartVisible(TargetPart, Character)) then continue end
+                -- // Vars
+                local PartPos, _ = WorldToViewportPoint(CurrentCamera, TargetPart.Position)
+                local Magnitude = (Vector2new(PartPos.X, PartPos.Y) - Vector2new(Mouse.X, Mouse.Y)).Magnitude
 
-                -- //
-                ClosestPlayer = Player
-                ShortestDistance = Magnitude
+                -- // Check if is in FOV
+                if (circle.Radius > Magnitude and Magnitude < ShortestDistance) then
+                    -- // Check if Visible
+                    if (ValiantAimHacks.VisibleCheck and not ValiantAimHacks.isPartVisible(TargetPart, Character)) then continue end
+
+                    -- //
+                    ClosestPlayer = Player
+                    ShortestDistance = Magnitude
+                end
             end
         end
     end
 
     -- // End
     ValiantAimHacks.Selected = (Chance and ClosestPlayer or LocalPlayer)
+    ValiantAimHacks.SelectedPart = (Chance and TargetPart or nil)
 end
 
 -- // Heartbeat Function
