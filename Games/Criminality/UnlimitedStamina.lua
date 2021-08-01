7633be0e0e010e425785af1963357cca79c9148e
Stefanuk12 <stefanukpadd@gmail.com>
2021-08-01 23:06:15 +0100

    So it works when you die

--- Games/Criminality/UnlimitedStamina.lua
+++ Games/Criminality/UnlimitedStamina.lua
@@ -1,30 +1,48 @@
--- // Get the stamina/walkspeed table
-local u110 = getrenv()._G.S_Get
-local u24 = debug.getupvalue(u110, 2)
+-- // Services
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
--- //
-local set = setmetatable({WS = 16}, {
-    -- // Spoofing value
-    __index = function(t, k)
-        -- // See when it tries to index S - Stamina
-        if (k == "S") then
-            -- // Return our spoofed value
-            return 100
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+local CharStats = ReplicatedStorage.CharStats
+
+-- // the main thing
+local function initialiseInfiniteStamina()
+    -- // Get the stamina/walkspeed table
+    local u110 = getrenv()._G.S_Get
+    local u24 = debug.getupvalue(u110, 2)
+
+    -- //
+    local set = setmetatable({WS = 16}, {
+        -- // Spoofing value
+        __index = function(t, k)
+            -- // See when it tries to index S - Stamina
+            if (k == "S") then
+                -- // Return our spoofed value
+                return 100
+            end
+
+            -- // Return
+            return rawget(t, k)
+        end,
+
+        -- // So it cannot set the S attribute
+        __newindex = function(t, k, v)
+            return
         end
+    })
 
-        -- // Return
-        return rawget(t, k)
-    end,
+    -- // Set the new table
+    debug.setupvalue(u24, 1, set)
+    debug.setupvalue(u24, 2, set)
+end
+initialiseInfiniteStamina()
 
-    -- // So it cannot set the S attribute
-    __newindex = function(t, k, v)
-        return
-    end
-})
+-- // So it works when you die
+LocalPlayer.CharacterAdded:Connect(function(Character)
+    -- // Wait until we have fully spawned in
+    Character:WaitForChild("Humanoid")
 
--- // Set the new table
-debug.setupvalue(u24, 1, set)
-debug.setupvalue(u24, 2, set)
-
--- // Print
-print("Infinite Stamina Done!")
+    -- // Initialise
+    initialiseInfiniteStamina()
+end)
