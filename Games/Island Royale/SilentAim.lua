e1ae4941ad15de03cf6e4146a9271c888b608abf
Stefanuk12 <stefanukpadd@gmail.com>
2021-08-04 19:33:26 +0100

    Initial Commit

---
+++ Games/Island Royale/SilentAim.lua
@@ -0,0 +1,35 @@
+-- // Dependencies
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Module.lua"))
+
+-- // Services
+local Workspace = game:GetService("Workspace")
+
+-- // Vars
+local CurrentCamera = Workspace.CurrentCamera
+local MapStuff = {Workspace.Map_Objects, Workspace.MapBase}
+
+-- // Hook
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    -- // Vars
+    local args = {...}
+    local method = getnamecallmethod()
+
+    -- //
+    if (method == "FindPartOnRayWithIgnoreList" and getfenv(2).script.Name == "RC" and Aiming.Check()) then
+        -- // Vars
+        local SelectedPart = Aiming.SelectedPart
+        local CurrentCameraPosition = CurrentCamera.CFrame.Position
+        local Direction = (SelectedPart.Position - CurrentCameraPosition)
+
+        -- // Overriding arguments
+        args[1] = Ray.new(CurrentCameraPosition, Direction.Unit * Direction.Magnitude)
+        args[2] = MapStuff
+
+        -- //
+        return __namecall(self, unpack(args))
+    end
+
+    -- //
+    return __namecall(self, ...)
+end)
