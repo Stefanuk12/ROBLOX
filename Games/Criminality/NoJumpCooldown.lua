eb41082095490cccd650dbd6fbeae88517f2fb08
Stefanuk12 <stefanukpadd@gmail.com>
2021-08-04 17:31:45 +0100

    Extra checks

--- Games/Criminality/NoJumpCooldown.lua
+++ Games/Criminality/NoJumpCooldown.lua
@@ -1,8 +1,14 @@
+-- // Services
+local Players = game:GetService("Players")
+
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+
 -- // Hooking __newindex
 local __newindex
 __newindex = hookmetamethod(game, "__newindex", function(t, k, v)
     -- // See if the game is trying to make it so we cannot jump :(
-    if (t.Name == "Humanoid" and k == "Jump" and v == false) then
+    if (t:IsDescendantOf(LocalPlayer.Character) and k == "Jump" and v == false) then
         -- // bad game
         return
     end
