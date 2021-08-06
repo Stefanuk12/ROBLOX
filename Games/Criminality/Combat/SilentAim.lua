e3cc48e75970fee5eb1373c565cb1ddffe437bb9
Stefanuk12 <stefanukpadd@gmail.com>
2021-08-06 16:23:06 +0100

    Okay should work now

--- Games/Criminality/Combat/SilentAim.lua
+++ Games/Criminality/Combat/SilentAim.lua
@@ -1,18 +1,29 @@
 -- // Load Silent Aim
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Module.lua"))()
+Aiming.TeamCheck(false)
+
+-- // Services
+local Workspace = game:GetService("Workspace")
+
+-- // Vars
+local CurrentCamera = Workspace.CurrentCamera
 
 -- // Hook
 local __index
-__index = hookmetamethod(game, "__index", (function(t, k)
+__index = hookmetamethod(game, "__index", function(t, k)
+    -- // Vars
+    local callingscript = getcallingscript()
+
     -- // Check if it trying to get our mouse's hit or target
-    if (t:IsA("Mouse") and (k == "Hit" or k == "Target")) then
+    if (t:IsA("Mouse") and (k == "X" or k == "Y") and tostring(callingscript) ~= "MouseScript") then
         -- // If we can use the silent aim
         if (Aiming.Check()) then
             -- // Vars
             local TargetPart = Aiming.SelectedPart
+            local vector, _ = CurrentCamera:WorldToScreenPoint(TargetPart.Position)
 
             -- // Return modded val
-            return (k == "Hit" and TargetPart.CFrame or TargetPart)
+            return (k == "X" and vector.X or vector.Y)
         end
     end
 
