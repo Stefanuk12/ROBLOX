bc4695e5e10229c572aeee0e590a532952197e42
Stefanuk12 <stefanukpadd@gmail.com>
2021-08-22 14:20:33 +0100

    Initial Commit

---
+++ Games/Dawn of Aurora/SilentAim.lua
@@ -0,0 +1,33 @@
+-- // Load Aiming Module
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Module.lua"))()
+
+-- // Services
+local Workspace = game:GetService("Workspace")
+
+-- // Vars
+local CurrentCamera = Workspace.CurrentCamera
+
+-- // Hook
+local __index
+__index = hookmetamethod(game, "__index", function(t, k)
+    -- // Check if it trying to get our mouse's unitray and see if we can use the aiming
+    if (not checkcaller() and t:IsA("Mouse") and k == "UnitRay" and Aiming.Check()) then
+        -- // Vars
+        local TargetPart = Aiming.SelectedPart
+
+        -- // Ray Vars
+        local Origin = CurrentCamera.CFrame.Position
+        local Destination = TargetPart.Position
+        local Direction = (Destination - Origin).Unit
+
+        -- // Create fake ray
+        local FakeRay = Ray.new(Origin, Direction)
+        print(FakeRay)
+
+        -- // Return it
+        return FakeRay
+    end
+
+    -- // Return
+    return __index(t, k)
+end)
