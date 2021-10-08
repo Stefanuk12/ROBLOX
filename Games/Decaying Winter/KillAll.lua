9eeb0c5a7e2c8c379f064a73697003d3139d3d61
Stefanuk12 <stefanukpadd@gmail.com>
2021-10-09 00:42:38 +0100

    ez

--- Games/Decaying Winter/KillAll.lua
+++ Games/Decaying Winter/KillAll.lua
@@ -1,6 +1,3 @@
--- // Configuration
-
-
 -- // Services
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
@@ -26,13 +23,19 @@ do
     RemoteKey = debug.getupvalue(stagger, 6)
 end
 
+-- // Make sure got remote key
+if (not RemoteKey or RemoteKey == "") then
+    print("Could not get remote key")
+end
+
 -- // Kills a target
 local function KillTarget(Target, Part)
     -- // Vars
+    local Humanoid = Target:WaitForChild("Humanoid")
     local Data = {
         "meleedamage",
         Target,
-        100,
+        12,
         false,
         "Fist",
         false,
@@ -42,29 +45,37 @@ local function KillTarget(Target, Part)
     }
 
     -- //
-    dealDamage:FireServer(Data, nil, RemoteKey)
+    while (Humanoid.Health > 1) do wait()
+        dealDamage:FireServer(Data, nil, RemoteKey)
+    end
+end
+
+local function Kill(Target)
+    -- // Make sure is a target
+    if (Target.Name:find("AI")) then
+        KillTarget(Target, Target.Torso)
+        print("Killed: " .. Target.Name)
+    end
 end
 
 -- // Kills every target
 local function KillTargets()
-    -- // Loop through NoTarget
-    for _, child in ipairs(NoTarget:GetChildren()) do
-        -- // Make sure is a target
-        if (child.Name:find("AI")) then
-            KillTarget(child, child.Torso)
-        end
+    -- // table.foreachi doesn't want to work for some reason - thread issues, cannot yield.
+
+    -- // Kill all targets in NoTarget folder
+    for _, Target in ipairs(NoTarget:GetChildren()) do
+        Kill(Target)
     end
 
-    -- // Loop through activeHostiles
-    for _, child in ipairs(activeHostiles:GetChildren()) do
-        -- // Make sure is a target
-        if (child.Name:find("AI")) then
-            KillTarget(child, child.Torso)
-        end
+    -- // Kill all targets in activeHostiles folder
+    for _, Target in ipairs(activeHostiles:GetChildren()) do
+        Kill(Target)
     end
 end
 
--- // Constantly kill all
-while (true) do wait()
-    KillTargets()
-end
+-- // Kill all current hostiles
+KillTargets()
+
+-- // Kill all fugure hostiles
+NoTarget.ChildAdded:Connect(Kill)
+activeHostiles.ChildAdded:Connect(Kill)
