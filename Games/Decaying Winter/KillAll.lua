ae121b9f2f6e69fe802bda30b00c656d33812825
Stefanuk12 <stefanukpadd@gmail.com>
2021-10-09 17:09:19 +0100

    Update

--- Games/Decaying Winter/KillAll.lua
+++ Games/Decaying Winter/KillAll.lua
@@ -1,32 +1,13 @@
+-- // Dependencies
+local DecayHax = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Decaying%20Winter/GetRemoteKey.lua"))()
+
 -- // Services
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 
 -- // Vars
-local LocalPlayer = Players.LocalPlayer
-local Backpack = LocalPlayer.Backpack
-local mainHandler = Backpack:WaitForChild("mainHandler")
-
 local ServerStuff = Workspace.ServerStuff
 local dealDamage = ServerStuff.dealDamage
-
-local NoTarget = Workspace.NoTarget
-local activeHostiles = Workspace.activeHostiles
-
--- // Get the remote key
-local RemoteKey = ""
-do
-    -- // Vars
-    local stagger = getsenv(mainHandler).stagger
-
-    -- //
-    RemoteKey = debug.getupvalue(stagger, 6)
-end
-
--- // Make sure got remote key
-if (not RemoteKey or RemoteKey == "") then
-    print("Could not get remote key")
-end
 
 -- // Kills a target
 local function KillTarget(Target, Part)
@@ -46,13 +27,14 @@ local function KillTarget(Target, Part)
 
     -- //
     while (Humanoid.Health > 1) do wait()
-        dealDamage:FireServer(Data, nil, RemoteKey)
+        dealDamage:FireServer(Data, nil, DecayHax.RemoteKey, DecayHax.RemoteKey2)
     end
 end
 
 local function Kill(Target)
     -- // Make sure is a target
     if (Target.Name:find("AI")) then
+        Target = Target.npc.Value
         KillTarget(Target, Target:WaitForChild("Torso"))
         print("Killed: " .. Target.Name)
     end
@@ -60,16 +42,8 @@ end
 
 -- // Kills every target
 local function KillTargets()
-    -- // table.foreachi doesn't want to work for some reason - thread issues, cannot yield.
-
-    -- // Kill all targets in NoTarget folder
-    for _, Target in ipairs(NoTarget:GetChildren()) do
-        Kill(Target)
-    end
-
-    -- // Kill all targets in activeHostiles folder
-    for _, Target in ipairs(activeHostiles:GetChildren()) do
-        Kill(Target)
+    for _, Player in ipairs(Players:GetChildren()) do
+        Kill(Player)
     end
 end
 
@@ -77,5 +51,4 @@ end
 KillTargets()
 
 -- // Kill all fugure hostiles
-NoTarget.ChildAdded:Connect(Kill)
-activeHostiles.ChildAdded:Connect(Kill)
+Players.ChildAdded:Connect(Kill)
