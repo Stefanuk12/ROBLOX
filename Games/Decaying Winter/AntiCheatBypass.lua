0ad9664492b4d7af85cfc66c4bdcbb3971fbe400
Stefanuk12 <stefanukpadd@gmail.com>
2021-10-09 01:09:08 +0100

    ez

---
+++ Games/Decaying Winter/AntiCheatBypass.lua
@@ -0,0 +1,2 @@
+-- // This is just an anti-kick lol, thats all you need
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/AntiKick.lua"))()
---
+++ Games/Decaying Winter/UnlimitedAmmo.lua
@@ -0,0 +1,13 @@
+-- // Vars
+local Amount = 200
+
+-- // Loop through gc
+for _, v in ipairs(getgc(true)) do
+    -- // Make sure is a table and narrow it down to the ammo table
+    if (typeof(v) == "table" and rawget(v, "Light") and rawget(v, "Small")) then
+        -- // Set Ammo
+        for AmmoType, _ in pairs(v) do
+            v[AmmoType] = Amount
+        end
+    end
+end
