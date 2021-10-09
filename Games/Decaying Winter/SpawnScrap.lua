2dcf1bff376574400984fc767b4d2b7fa1aadc19
Stefanuk12 <stefanukpadd@gmail.com>
2021-10-09 13:54:18 +0100

    Update warning

--- Games/Decaying Winter/SpawnScrap.lua
+++ Games/Decaying Winter/SpawnScrap.lua
@@ -1,7 +1,7 @@
--- // DO NOT GO OVER 1900 SCRAP, YOU WILL GET KICKED
+-- // DO NOT GO OVER 1910 SCRAP, YOU WILL GET KICKED
 
 -- // Configuration
-local DropAmount = 100
+local DropAmount = 100 -- // DO NOT GO OVER 1999, YOU WILL GET BANNED
 
 -- // Services
 local Workspace = game:GetService("Workspace")
