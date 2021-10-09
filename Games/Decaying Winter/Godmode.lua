62f6ba8d4b746bd49c76bc11ca0e7ce7f2681863
Stefanuk12 <stefanukpadd@gmail.com>
2021-10-09 01:58:39 +0100

    Update

--- Games/Decaying Winter/Godmode.lua
+++ Games/Decaying Winter/Godmode.lua
@@ -1,3 +1,8 @@
+-- // Information
+--[[
+    You need a bandage out in your hand
+]]
+
 -- // Services
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
@@ -20,7 +25,31 @@ do
     RemoteKey = debug.getupvalue(stagger, 6)
 end
 
--- // Constantly bandage up
-while (true) do wait()
-    dealDamage:FireServer("regenheal", nil, RemoteKey)
+-- // Heal enough health to get back to 100
+local function HealthFull(Health)
+    -- // Workout how many times to fire remote
+    local FireAmount = math.ceil((100 - Health) / 5)
+
+    -- // Fire remote
+    for i = 1, FireAmount do wait()
+        dealDamage:FireServer("regenheal", nil, RemoteKey)
+    end
 end
+
+-- //
+local function onCharacter(Character)
+    -- // Vars
+    local Humanoid = Character:WaitForChild("Humanoid")
+
+    -- // Heal to full
+    HealthFull(Humanoid.Health)
+
+    -- // See whenever lose health, heal back up
+    Humanoid.HealthChanged:Connect(function(Health)
+        HealthFull(Health)
+    end)
+end
+onCharacter(LocalPlayer.Character)
+
+-- // Work after death
+LocalPlayer.CharacterAdded:Connect(onCharacter)
