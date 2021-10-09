d6db5d56231b12a495a4d7540d152601f5f79078
Stefanuk12 <stefanukpadd@gmail.com>
2021-10-09 16:48:21 +0100

    Update

--- Games/Decaying Winter/GetRemoteKey.lua
+++ Games/Decaying Winter/GetRemoteKey.lua
@@ -1,17 +1,72 @@
+-- // Configuration
+if (not DecayHax) then
+    getgenv().DecayHax = {}
+end
+local DecayHax = getgenv().DecayHax
+
+-- // Dependencies
+local Signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Signal/main/Signal.lua"))()
+
 -- // Services
+local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 
 -- // Vars
 local LocalPlayer = Players.LocalPlayer
-local Backpack = LocalPlayer.Backpack
-local mainHandler = Backpack:WaitForChild("mainHandler")
+local Mouse = LocalPlayer:GetMouse()
+local ask = Workspace.ServerStuff.ask
 
--- // Get the remote key
-local RemoteKey = ""
-do
+-- // Events
+DecayHax.OnAsk = Signal.new("OnAsk")
+
+-- // Hook
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(...)
     -- // Vars
-    local stagger = getsenv(mainHandler).stagger
+    local args = {...}
+    local self = args[1]
+    local method = getnamecallmethod()
+
+    -- // See if is making new key
+    if (method == "InvokeServer" and self == ask) then
+        -- // Fire
+        DecayHax.OnAsk:Fire()
+    end
 
     -- //
-    RemoteKey = debug.getupvalue(stagger, 6)
+    return __namecall(...)
+end)
+
+-- // Set the remote key
+local function SetRemoteKeys()
+    -- // Vars
+    local Connections
+
+    -- // Make sure we got the connection
+    repeat wait()
+        Connections = getconnections(Mouse.Button2Down)
+    until #Connections > 0
+
+    -- // To get the function we need
+    for _, Connection in ipairs(Connections) do
+        -- // Vars
+        local ConnectionFunction = Connection.Function
+
+        -- // Set
+        DecayHax.RemoteKey = debug.getupvalue(ConnectionFunction, 8)
+        DecayHax.RemoteKey2 = debug.getupvalue(ConnectionFunction, 9)
+
+        -- //
+        break
+    end
+
+    -- //
+    print(DecayHax.RemoteKey, DecayHax.RemoteKey2)
 end
+SetRemoteKeys()
+
+-- // See whenever game makes a new key
+DecayHax.OnAsk:Connect(SetRemoteKeys)
+
+-- // Return
+return DecayHax
