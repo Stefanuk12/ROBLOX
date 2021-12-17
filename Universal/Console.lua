3d1ed9c906db74497286f947e4dbdd8900c9cedb
Stefanuk12 <stefanukpadd@gmail.com>
2021-12-17 17:14:35 +0000

    why not

---
+++ Universal/Console.lua
@@ -0,0 +1,37 @@
+-- // Dependencies
+local Chalk = loadstring(game:HttpGet("https://raw.githubusercontent.com/Belkworks/chalk/master/init.lua"))()
+
+-- // Vars
+local Types = {
+    print = "white",
+    warn = "yellow",
+    error = "red"
+}
+
+-- //
+local function OutputManager(Type, ...)
+    -- // Vars
+    local Colour = Chalk[Types[Type]]
+
+    -- //
+    local ToPrint = Colour(...)
+    Chalk.print(ToPrint)
+end
+
+-- // Loop through each type
+for Type, _ in pairs(Types) do
+    -- // Hook the type
+    local old
+    old = hookfunction(getgenv()[Type], function(...)
+        -- // See if we called
+        if (checkcaller()) then
+            -- //
+            OutputManager(Type, ...)
+            return
+        end
+
+        -- //
+        old(...)
+        return
+    end)
+end
