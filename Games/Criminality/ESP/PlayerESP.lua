b93bf0416ad93a11b3aaf2938b93284a1282ce9e
Stefanuk12 <stefanukpadd@gmail.com>
2021-12-21 22:10:39 +0000

    Updated

--- Games/Criminality/ESP/PlayerESP.lua
+++ Games/Criminality/ESP/PlayerESP.lua
@@ -1,5 +1,1 @@
--- // Load Module
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ESP/Rewrite.lua"))()
-
--- // Load Player ESP
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ESP/Example.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/ESP/Examples/PlayerESP.lua"))()
