e9bc2d1c03d16bab97704c7f8fbd5778d52de1fa
Stefanuk12 <stefanukpadd@gmail.com>
2021-12-24 18:29:06 +0000

    Added a load script

---
+++ Universal/Aiming/Load.lua
@@ -0,0 +1,13 @@
+-- // Vars
+local Format = "https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/%d.lua"
+
+-- // Attempt to load a patch for the game
+local Success, _ = pcall(loadstring, Format:format(game.PlaceId))
+
+-- // Load default otherwise
+if (Success) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Module.lua"))()
+end
+
+-- // Return
+return Aiming
