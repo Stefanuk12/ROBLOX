e8316ed5a7bd8b3b7da0c325a8caf2e225d3d355
Stefanuk12 <stefanukpadd@gmail.com>
2021-12-24 19:16:47 +0000

    Fixed

--- Universal/Aiming/Load.lua
+++ Universal/Aiming/Load.lua
@@ -2,7 +2,10 @@
 local Format = "https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/%d.lua"
 
 -- // Attempt to load a patch for the game
-local Success, _ = pcall(loadstring, Format:format(game.PlaceId))
+local Success, _ = pcall(function()
+    local URL = Format:format(game.PlaceId)
+    loadstring(game:HttpGet(URL))()
+end)
 
 -- // Load default otherwise
 if (not Success) then
