08e1f3e5b4d50be2e676f990c714fd18d7998016
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-12-31 23:01:49 +0000

    Fix

--- Games/Rush Point/AntiCheatBypass.lua
+++ Games/Rush Point/AntiCheatBypass.lua
@@ -9,13 +9,15 @@ hookfunction(WeaponManagerClient.ConnectCharacterChecker, function() end)
 
 -- // Hook the current connected funcs
 for _, v in ipairs(getreg()) do
-    -- // Vars
-    local constants = debug.getconstants(v)
+    -- // Make sure is function
+    if (typeof(v) == "function") then
+        -- // Vars
+        local constants = debug.getconstants(v)
 
-    -- // Check constants
-    if (not is_synapse_function(v) and islclosure(v) and table.find(constants, "HipHeight") and table.find(constants, 3.1)) then
-        -- // Hook
-        print(v)
-        hookfunction(v, function() end)
+        -- // Check constants
+        if (not is_synapse_function(v) and islclosure(v) and table.find(constants, "HipHeight") and table.find(constants, 3.1)) then
+            -- // Hook
+            hookfunction(v, function() end)
+        end
     end
 end
