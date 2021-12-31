9437ba8aaf9f235d7d79bcdf88ea02d0c504c267
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-12-31 23:58:24 +0000

    ez

---
+++ Games/Rush Point/AimLock.lua
@@ -0,0 +1,5 @@
+-- // Dependencies
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/RushPoint.lua"))()
+
+-- // Start
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Examples/AimLock.lua"))()
