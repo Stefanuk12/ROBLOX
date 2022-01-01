d920bb16c6437d1d1c77590020e43da45d18112b
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-01-01 01:45:17 +0000

    Added own "settings" area

--- Universal/Aiming/Examples/AimLock.lua
+++ Universal/Aiming/Examples/AimLock.lua
@@ -6,16 +6,20 @@ local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 
 -- // Vars
-local Keybind = Enum.UserInputType.MouseButton2 -- // You can also have Enum.KeyCode.E, etc.
+Aiming.AimLock = {
+    Enabled = true,
+    Keybind = Enum.UserInputType.MouseButton2, -- // You can also have Enum.KeyCode.E, etc.
+}
+local Settings = Aiming.AimLock
 
 -- //
-local function ShouldUseCamera()
+function Settings.ShouldUseCamera()
     -- //
     return (UserInputService.MouseBehavior == Enum.MouseBehavior.LockCenter)
 end
 
 -- // Allows for custom
-function Aiming.AimLockPosition(CameraMode)
+function Settings.AimLockPosition(CameraMode)
     local Position = CameraMode and Aiming.SelectedPart.Position or Aiming.SelectedPosition
     return Position, {}
 end
@@ -23,13 +27,14 @@ end
 -- // Constantly run
 RunService:BindToRenderStep("AimLockAiming", 0, function()
     -- // Vars
+    local Keybind = Settings.Keybind
     local IsToggled = (Keybind.EnumType == Enum.KeyCode and UserInputService:IsKeyDown(Keybind) or UserInputService:IsMouseButtonPressed(Keybind))
 
     -- // Make sure key (or mouse button) is down
-    if (IsToggled and Aiming.Check()) then
+    if (Settings.Enabled and IsToggled and Aiming.Check()) then
         -- // Vars
-        local CameraMode = ShouldUseCamera()
-        local Position, BeizerData = Aiming.AimLockPosition(CameraMode)
+        local CameraMode = Settings.ShouldUseCamera()
+        local Position, BeizerData = Settings.AimLockPosition(CameraMode)
 
         -- // Aim with camera
         if (CameraMode) then
