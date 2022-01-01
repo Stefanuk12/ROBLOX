21287e2803f73789f06b1277b8f727a3dcf0310f
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-01-01 00:34:25 +0000

    now works

--- Games/Rush Point/SilentAim.lua
+++ Games/Rush Point/SilentAim.lua
@@ -1,29 +1,25 @@
 -- // Dependencies
-local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/Load.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/RushPoint.lua"))()
 
 -- // Services
 local Workspace = game:GetService("Workspace")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- // Vars
 local CurrentCamera = Workspace.CurrentCamera
-local WeaponManagerClient = require(ReplicatedStorage.Modules.Client.Managers.WeaponManagerClient)
+local CFramelookAt = CFrame.lookAt
 
 -- // Hook
 local __index
-__index = hookfunction(game, "__index", function(t, k)
-    -- // Vars
-    local callingscript = getcallingscript()
+__index = hookmetamethod(game, "__index", function(t, k)
+    -- // Make sure it is the shoot function trying to get the camera's cframe
+    if (not checkcaller() and t == CurrentCamera and k == "CFrame" and debug.validlevel(3) and #debug.getupvalues(3) == 11 and Aiming.Check()) then
+        local Origin = __index(t, k).Position
+        local Destination = Aiming.SelectedPart.Position
 
-    -- // Check that the weapon manager's shoot function is trying to get the camera's cframe
-    if (not checkcaller() and callingscript == WeaponManagerClient and t == CurrentCamera and k == "CFrame" and Aiming.Check() and #debug.getupvalues(2) == 1) then
-        -- // Vars
-        local Origin = CurrentCamera.CFrame.Position
-
-        -- // Return modified ver
-        return CFrame.lookAt(Origin, Aiming.SelectedPart.Position)
+        local Modified = CFramelookAt(Origin, Destination)
+        return Modified
     end
 
-    -- //
+    -- // Return old
     return __index(t, k)
 end)
