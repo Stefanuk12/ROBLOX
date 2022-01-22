199a18531d6280c3a042855739ce1cda08c04296
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-01-22 12:28:08 +0000

    Initial Commit

---
+++ Games/Obliteration Orbs/AutoRankUp.lua
@@ -0,0 +1,24 @@
+-- // Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+
+local KillsNeeded = LocalPlayer.PlayerGui["Kils Required GUI"].Frame.Kills
+local Kills = LocalPlayer.leaderstats.Kills
+local Events = ReplicatedStorage.Events
+
+-- // See when we get a kill
+Kills.Changed:Connect(function(value)
+    -- // Vars
+    local Needed = tonumber(KillsNeeded.Text)
+
+    -- // Make sure we have enough
+    if not (value >= Needed) then
+        return
+    end
+
+    -- // Attempt rank up
+    Events.AttemptToRankUp_Event:FireServer()
+end)
