8eda1d2c6c36e7e3c0ae75d99642d644fae2d7f0
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-02-23 11:37:22 +0000

    Added signals

--- Other/CrossCommunication.lua
+++ Other/CrossCommunication.lua
@@ -8,6 +8,10 @@
     If another person were to execute, this cycle would repeat.
 ]]
 
+-- // Dependencies
+local SignalManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Signal/main/Manager.lua"))()
+local Manager = SignalManager.new()
+
 -- // Configuration
 local Secret = "hello comrade"
 
@@ -18,7 +22,11 @@ local Players = game:GetService("Players")
 local Comrades = {}
 local AllChatType = Enum.PlayerChatType.All
 
--- //
+-- // Create signals
+Manager:Add("ExploiterJoined")
+Manager:Add("ExploiterLeaving")
+
+-- // See when people chat
 local ChatConnection
 ChatConnection = Players.PlayerChatted:Connect(function(ChatType, Player, Message, TargetPlayer)
     -- // Make sure message matches and we have not already gotten the user
@@ -29,9 +37,32 @@ ChatConnection = Players.PlayerChatted:Connect(function(ChatType, Player, Messag
         -- // Chat
         Players:Chat(Secret)
 
-        -- //
-        print("Found a fellow comrade, welcome: " .. Player.Name)
+        -- // Fire
+        Manager:Fire("ExploiterJoined", Player)
     end
+end)
+
+-- // See when people leave
+local PlayerConnection
+PlayerConnection = Players.PlayerRemoving:Connect(function(Player)
+    -- // Check if found
+    local i = table.find(Comrades, Player)
+    if (i) then
+        -- // Remove
+        table.remove(Comrades, i)
+
+        -- // Fire
+        Manager:Fire("ExploiterLeaving", Player)
+    end
+end)
+
+-- // Connect to signals
+Manager:Connect("ExploiterJoined", function(Player)
+    print("Hello fellow comrade: " .. Player.Name)
+end)
+
+Manager:Connect("ExploiterLeaving", function(Player)
+    print("Goodbye fellow comrade: " .. Player.Name .. " :(")
 end)
 
 -- // Chat the secret
