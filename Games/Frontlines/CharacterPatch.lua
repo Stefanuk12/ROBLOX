8fdf522c4efbb25d0dcf321c059330f22d4c4e2f
Babyhamsta <Babyhamstaz@yahoo.com>
2022-02-27 11:00:21 -0600

    Update CharacterPatch.lua
    Sets up 3 teams and forces team color rather than using the ids. This fixes the team color issues.

--- Games/Frontlines/CharacterPatch.lua
+++ Games/Frontlines/CharacterPatch.lua
@@ -8,63 +8,78 @@ local Teams = {}
 
 -- //
 local function GrabPlayerId(Player)
-    -- // Loop through all the players
-    for i, v in pairs(CLIPlayers) do
-        -- // See if the players match
-        if (v == Player) then
-            -- // Return their player id
-            return i
-        end
-    end
+   -- // Loop through all the players
+   for i, v in pairs(CLIPlayers) do
+       -- // See if the players match
+       if (v == Player) then
+           -- // Return their player id
+           return i
+       end
+   end
 end
+
+-- //
+local function CreateTeams()
+   -- // There are always 3 (0 = Lobby, 1 = Blue, 2 = Red)
+   
+   -- // Lobby
+   local Team0 = Instance.new("Team")
+   Team0.TeamColor = BrickColor.Gray()
+   Team0.Name = "0"
+   Teams["0"] = Team0
+   
+   -- // Blue
+   local Team1 = Instance.new("Team")
+   Team1.TeamColor = BrickColor.Blue()
+   Team1.Name = "1"
+   Teams["1"] = Team1
+   
+   -- // Red
+   local Team2 = Instance.new("Team")
+   Team2.TeamColor = BrickColor.Red()
+   Team2.Name = "2"
+   Teams["2"] = Team2
+end
+
+-- // Lets go ahead and make those.
+CreateTeams();
 
 -- //
 local __index
 __index = hookmetamethod(game, "__index", function(t, k)
-    -- // Make sure the game cannot access this
-    if (checkcaller()) then
-        -- // Check if we are trying to get a player's character, team, or teamcolor
-        if (t:IsA("Player") and (k == "Character" or k == "Team" or k == "TeamColor")) then
-            -- // Vars
-            local PlayerId = GrabPlayerId(t)
+   -- // Make sure the game cannot access this
+   if (checkcaller()) then
+       -- // Check if we are trying to get a player's character, team, or teamcolor
+       if (t:IsA("Player") and (k == "Character" or k == "Team" or k == "TeamColor")) then
+           -- // Vars
+           local PlayerId = GrabPlayerId(t)
 
-            -- // Make sure we got the id
-            if (not PlayerId) then
-                return nil
-            end
+           -- // Make sure we got the id
+           if (not PlayerId) then
+               return nil
+           end
 
-            -- // Return their character
-            if (k == "Character") then
-                return ModelInstances[PlayerId]
-            end
+           -- // Return their character
+           if (k == "Character") then
+               return ModelInstances[PlayerId]
+           end
 
-            -- // Return their team
-            if (k == "Team" or k == "TeamColor") then
-                -- // Vars
-                local PlayerTeam = CLITeams[PlayerId]
+           -- // Return their team
+           if (k == "Team" or k == "TeamColor") then
+               -- // Vars
+               local PlayerTeam = CLITeams[PlayerId]
 
-                -- // Check if the team exists
-                if (not Teams[PlayerTeam]) then
-                    -- // Create a new team
-                    local Team = Instance.new("Team")
-                    Team.TeamColor = BrickColor.new(PlayerTeam)
-                    Team.Name = PlayerTeam
+               -- // Return team
+               local ResolvedTeam = Teams[tostring(PlayerTeam)]
+               if (k == "Team") then
+                   return ResolvedTeam
+               else
+                   return ResolvedTeam.TeamColor
+               end
+           end
+       end
+   end
 
-                    -- // Add it
-                    Teams[PlayerTeam] = Team
-                end
-
-                -- // Return team
-                local ResolvedTeam = Teams[PlayerTeam]
-                if (k == "Team") then
-                    return ResolvedTeam
-                else
-                    return ResolvedTeam.TeamColor
-                end
-            end
-        end
-    end
-
-    -- //
-    return __index(t, k)
+   -- //
+   return __index(t, k)
 end)
