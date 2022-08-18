e844b1ca4448cb2db29ad0ad769cb198b2eb4db7
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-08-18 12:14:54 +0100

    bruh

---
+++ Other/ChattedFix.lua
@@ -0,0 +1,30 @@
+--[[
+    Information:
+
+    Fixes old .Chatted scripts by firing it when OnMessageDoneFiltering is called instead.
+
+    Simply put in autoexec.
+]]
+
+-- // Services
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+
+-- // Connect to the "new event"
+ReplicatedStorage.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(function(Data)
+    -- // Get all of the data
+    local Player = Players[Data.FromSpeaker]
+    local Message = Data.Message
+    local Channel = Data.OriginalChannel
+
+    -- // Ignore if it is LocalPlayer as that event already fires
+    if (Player == LocalPlayer) then
+        return
+    end
+
+    -- // Fire
+    firesignal(Player.Chatted, Message, Channel)
+end)
