374561c9f6555701fe7c1b3835c7d8a356d26652
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-04-17 19:59:29 +0100

    Improved

--- Games/Da Hood/AntiCheatBypass.lua
+++ Games/Da Hood/AntiCheatBypass.lua
@@ -1,13 +1,14 @@
+-- // Ensure is Da Hood (put in autoexec)
+if (game.PlaceId ~= 2788229376) then
+    return
+end
+
 -- // Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- // Vars
 local tablefind = table.find
 local MainEvent = ReplicatedStorage.MainEvent
-local SpoofTable = {
-    WalkSpeed = 16,
-    JumpPower = 50
-}
 
 -- // Configuration
 local Flags = {
@@ -23,7 +24,6 @@ __namecall = hookmetamethod(game, "__namecall", function(...)
     local args = {...}
     local self = args[1]
     local method = getnamecallmethod()
-    local caller = getcallingscript()
 
     -- // See if the game is trying to alert the server
     if (method == "FireServer" and self == MainEvent and tablefind(Flags, args[2])) then
@@ -32,41 +32,25 @@ __namecall = hookmetamethod(game, "__namecall", function(...)
 
     -- // Anti Crash
     if (not checkcaller() and getfenv(2).crash) then
-        -- // Hook the crash function to make it not work
-        hookfunction(getfenv(2).crash, function()
-            warn("Crash Attempt") 
-        end)
+        -- // Set the crash function (hooking can cause stutters)
+        local fenv = getfenv(2)
+        fenv.crash = function() end
+        setfenv(2, fenv)
     end
-    
+
     -- //
     return __namecall(...)
 end)
 
--- // __index hook
-local __index
-__index = hookmetamethod(game, "__index", function(t, k)
-    -- // Make sure it's trying to get our humanoid's ws/jp
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        -- // Return spoof values
-        return SpoofTable[k]
-    end
-
-    -- //
-    return __index(t, k)
-end)
-
--- // __newindex hook
+-- // __newindex hook (stops game from setting ws/jp)
 local __newindex
 __newindex = hookmetamethod(game, "__newindex", function(t, k, v)
     -- // Make sure it's trying to set our humanoid's ws/jp
     if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
-        -- // Add values to spoof table
-        SpoofTable[k] = v
-
         -- // Disallow the set
         return
     end
-    
+
     -- //
     return __newindex(t, k, v)
 end)
